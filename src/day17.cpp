f1714b4f5a36476c88ab4d9da43c169eece00b8e
Julian Bruyat <bruyat.ju@live.fr>
2020-12-18 05:11:46 +0100

    Day 17 - isolate Position

--- src/day17.cpp
+++ src/day17.cpp
@@ -8,52 +8,23 @@
 
 // https://adventofcode.com/2020/day/17
 
-// Hard coding 3D / 4D is too mainstream.
-// So here it is : generalization of game of life to any dimensional space.
-
-/// Currently spported bounds for a dimension
-struct Dimension {
-    size_t offset_of_zero;  // -offset_of_zero == min_length
-    size_t max_length;      // exclusive
-};
-
-// Supported bounds for each dimension
-template <size_t NB_DIM>
-struct SpaceSize : public std::array<Dimension, NB_DIM> {  
-    [[nodiscard]] SpaceSize next() const noexcept {
-        SpaceSize retval = *this;
-
-        for (size_t i = 0 ; i != NB_DIM ; ++i) {
-            ++retval[i].offset_of_zero;
-            ++retval[i].max_length;
-        }
-
-        return retval;
-    }
-
-    [[nodiscard]] size_t size() const noexcept {
-        size_t n = 1;
-
-        for (size_t i = 0 ; i != NB_DIM ; ++i) {
-            n *= (*this)[i].offset_of_zero + (*this)[i].max_length;
-        }
-
-        return n;
-    }
-};
-
 /// A position in the NB_DIM D space
 template <size_t NB_DIM>
 struct Position {
     std::array<int, NB_DIM> m_coordinate;
 
-    Position() {
-        m_coordinate.fill(0);
+    Position() { m_coordinate.fill(0); }
+
+    [[nodiscard]] constexpr bool operator<(const Position & other) const {
+        for (size_t i = 0 ; i != NB_DIM ; ++i) {
+            if (m_coordinate[i] < other.m_coordinate[i]) return true;
+            if (m_coordinate[i] > other.m_coordinate[i]) return false;
+        }
+
+        return false;
     }
 
-    Position(size_t pos, const SpaceSize<NB_DIM> & space);
-
-    constexpr bool operator==(const Position & other) {
+    [[nodiscard]] constexpr bool operator==(const Position & other) const {
         return m_coordinate == other.m_coordinate;
     }
 
@@ -70,35 +41,13 @@ struct Position {
         return copy;
     }
 
-    [[nodiscard]] constexpr size_t to_array_position(SpaceSize<NB_DIM> space_size) const {
-        size_t my_position = 0;
-
-        for (size_t i = 0 ; i != NB_DIM ; ++i) {
-            int in_my_dimension = m_coordinate[i] + space_size[i].offset_of_zero;
-            my_position = my_position * (space_size[i].offset_of_zero + space_size[i].max_length) + in_my_dimension;
-        }
-
-        return my_position;
-    }
-
-    [[nodiscard]] constexpr std::optional<size_t> to_array_position_maybe(SpaceSize<NB_DIM> space_size) const {
-        size_t my_position = 0;
-
-        for (size_t i = 0 ; i != NB_DIM ; ++i) {
-            if (m_coordinate[i] < 0 && static_cast<size_t>(-m_coordinate[i]) > space_size[i].offset_of_zero) return std::nullopt;
-            if (m_coordinate[i] > 0 && static_cast<size_t>(m_coordinate[i]) >= space_size[i].max_length) return std::nullopt;
-
-            int in_my_dimension = m_coordinate[i] + space_size[i].offset_of_zero;
-            my_position = my_position * (space_size[i].offset_of_zero + space_size[i].max_length) + in_my_dimension;
-        }
-
-        return my_position;
-    }
-
     friend std::ostream & operator<<(std::ostream & stream, const Position<NB_DIM> & position) {
+        stream << '('
         for (size_t i = 0 ; i != NB_DIM ; ++i) {
-            stream << position.m_coordinate[i] << ' ';
+            if (i != 0) stream << ", ";
+            stream << position.m_coordinate[i];
         }
+        stream << ')';
         return stream;
     }
 
@@ -140,100 +89,164 @@ struct Position {
     }
 };
 
-template <size_t NB_DIM>
-Position<NB_DIM>::Position(size_t pos, const SpaceSize<NB_DIM> & space) {
-    for (size_t i = 0 ; i != NB_DIM ; ++i) {
-        size_t real_i = NB_DIM - 1 - i;
-        size_t dim_size = space[real_i].offset_of_zero + space[real_i].max_length;
-        m_coordinate[real_i]  = pos % dim_size;
-        m_coordinate[real_i] -= space[real_i].offset_of_zero;
 
-        pos = pos / dim_size;
-    }
-}
+namespace vector_implementation {
+    // Hard coding 3D / 4D is too mainstream.
+    // So here it is : generalization of game of life to any dimensional space.
 
-/// A NB_DIM world
-template <size_t NB_DIM>
-struct Field {
-    std::vector<char> m_symbols;
-    SpaceSize<NB_DIM> m_spacesize;
+    /// Currently spported bounds for a dimension
+    struct Dimension {
+        size_t offset_of_zero;  // -offset_of_zero == min_length
+        size_t max_length;      // exclusive
+    };
 
-    Field(const std::vector<std::string> & lines);
+    // Supported bounds for each dimension
+    template <size_t NB_DIM>
+    struct SpaceSize : public std::array<Dimension, NB_DIM> {  
+        [[nodiscard]] SpaceSize next() const noexcept {
+            SpaceSize retval = *this;
 
-    void next();
-
-    [[nodiscard]] test::Value count_occupied() const noexcept {
-        return std::count_if(m_symbols.begin(), m_symbols.end(), 
-            [](char c) { return c == '#'; }
-        );
-    }
-
-    char get_at(Position<NB_DIM> position) {
-        const auto maybe = position.to_array_position_maybe(m_spacesize);
-        if (!maybe) return '.';
-
-        return m_symbols[*maybe];
-    }
-};
-
-template <size_t NB_DIM>
-Field<NB_DIM>::Field(const std::vector<std::string> & lines) {
-    m_spacesize.fill(Dimension{ 0, 1 });
-    m_spacesize[0] = Dimension{ 0, lines[0].size() };
-    m_spacesize[1] = Dimension{ 0, lines.size() };
-
-    for (size_t y = 0 ; y != lines.size() ; ++y) {
-        for (size_t x = 0 ; x != lines[y].size() ; ++x) {
-            // Position<NB_DIM> position;
-            // position.m_coordinate[1] = x;
-            // position.m_coordinate[0] = y;
-            // const size_t pos = position.to_array_position(m_spacesize);
-            // assert(pos == m_symboles.size());
-            m_symbols.push_back(lines[y][x]);
-        }
-    }
-}
-
-template <size_t NB_DIM>
-void Field<NB_DIM>::next() {
-    std::vector<char> next_symbols;
-    SpaceSize<NB_DIM> next_spacesize = m_spacesize.next();
-
-    const size_t size = next_spacesize.size();
-    for (size_t i = 0 ; i != size ; ++i) {
-        Position p = Position(i, next_spacesize);
-        char my_position = get_at(p);
-
-        size_t occ = 0;
-
-        p.for_each_neighbour_position(
-            [&](Position<NB_DIM> neighbour) {
-                if (get_at(neighbour) == '#') {
-                    occ += 1;
-                }
+            for (size_t i = 0 ; i != NB_DIM ; ++i) {
+                ++retval[i].offset_of_zero;
+                ++retval[i].max_length;
             }
-        );
 
-        if (my_position == '#') {
-            next_symbols.push_back((occ == 2 || occ == 3) ? '#' : '.');
-        } else {
-            next_symbols.push_back(             occ == 3  ? '#' : '.');
+            return retval;
+        }
+
+        [[nodiscard]] size_t size() const noexcept {
+            size_t n = 1;
+
+            for (size_t i = 0 ; i != NB_DIM ; ++i) {
+                n *= (*this)[i].offset_of_zero + (*this)[i].max_length;
+            }
+
+            return n;
+        }
+
+        [[nodiscard]] Position<NB_DIM> to_position(size_t array_index) const {
+            Position<NB_DIM> position;
+
+            for (size_t i = 0 ; i != NB_DIM ; ++i) {
+                size_t real_i = NB_DIM - 1 - i;
+                size_t dim_size = (*this)[real_i].offset_of_zero + (*this)[real_i].max_length;
+                position.m_coordinate[real_i]  = array_index % dim_size;
+                position.m_coordinate[real_i] -= (*this)[real_i].offset_of_zero;
+
+                array_index = array_index / dim_size;
+            }
+
+            return position;
+        }
+
+        [[nodiscard]] constexpr size_t to_array_index(const Position<NB_DIM> & position) const {
+            return to_array_index_maybe(position).value();
+        }
+
+        [[nodiscard]] constexpr std::optional<size_t> to_array_index_maybe(const Position<NB_DIM> & position) const {
+            size_t index = 0;
+
+            const auto & coordinate = position.m_coordinate;
+    
+            for (size_t i = 0 ; i != NB_DIM ; ++i) {
+                const Dimension & dim = (*this)[i];
+
+                if (coordinate[i] < 0 && static_cast<size_t>(-coordinate[i]) >  dim.offset_of_zero) return std::nullopt;
+                if (coordinate[i] > 0 && static_cast<size_t>( coordinate[i]) >= dim.max_length) return std::nullopt;
+    
+                int in_my_dimension = coordinate[i] + dim.offset_of_zero;
+                index = index * (dim.offset_of_zero + dim.max_length) + in_my_dimension;
+            }
+    
+            return index;
+        }
+    };
+
+    /// A NB_DIM world
+    template <size_t NB_DIM>
+    struct Field {
+        std::vector<char> m_symbols;
+        SpaceSize<NB_DIM> m_spacesize;
+
+        Field(const std::vector<std::string> & lines);
+
+        void next();
+
+        [[nodiscard]] test::Value count_occupied() const noexcept {
+            return std::count_if(m_symbols.begin(), m_symbols.end(), 
+                [](char c) { return c == '#'; }
+            );
+        }
+
+        char get_at(const Position<NB_DIM> & position) {
+            const auto maybe = m_spacesize.to_array_index_maybe(position);
+            if (!maybe) return '.';
+
+            return m_symbols[*maybe];
+        }
+    };
+
+    template <size_t NB_DIM>
+    Field<NB_DIM>::Field(const std::vector<std::string> & lines) {
+        m_spacesize.fill(Dimension{ 0, 1 });
+        m_spacesize[0] = Dimension{ 0, lines[0].size() };
+        m_spacesize[1] = Dimension{ 0, lines.size() };
+
+        for (size_t y = 0 ; y != lines.size() ; ++y) {
+            for (size_t x = 0 ; x != lines[y].size() ; ++x) {
+                // Position<NB_DIM> position;
+                // position.m_coordinate[1] = x;
+                // position.m_coordinate[0] = y;
+                // const size_t pos = position.to_array_position(m_spacesize);
+                // assert(pos == m_symboles.size());
+                m_symbols.push_back(lines[y][x]);
+            }
         }
     }
 
-    m_symbols = next_symbols;
-    m_spacesize = next_spacesize;
+    template <size_t NB_DIM>
+    void Field<NB_DIM>::next() {
+        std::vector<char> next_symbols;
+        const SpaceSize<NB_DIM> next_spacesize = m_spacesize.next();
+
+        const size_t size = next_spacesize.size();
+        for (size_t i = 0 ; i != size ; ++i) {
+            const Position p = next_spacesize.to_position(i);
+            char my_position = get_at(p);
+
+            size_t occ = 0;
+
+            p.for_each_neighbour_position(
+                [&](Position<NB_DIM> neighbour) {
+                    if (get_at(neighbour) == '#') {
+                        occ += 1;
+                    }
+                }
+            );
+
+            if (my_position == '#') {
+                next_symbols.push_back((occ == 2 || occ == 3) ? '#' : '.');
+            } else {
+                next_symbols.push_back(             occ == 3  ? '#' : '.');
+            }
+        }
+
+        m_symbols = next_symbols;
+        m_spacesize = next_spacesize;
+    }
+
+    /// What the exercice was again?
+    template<size_t NB_DIM>
+    static test::Value occupied_after_six_iterations(const std::vector<std::string> & lines) {
+        Field<NB_DIM> field { lines };
+
+        for (size_t i = 0 ; i != 6 ; ++i) field.next();
+
+        return field.count_occupied();
+    }
 }
 
-/// What the exercice was again?
-template<size_t NB_DIM>
-static test::Value occupied_after_six_iterations(const std::vector<std::string> & lines) {
-    Field<NB_DIM> field { lines };
-
-    for (size_t i = 0 ; i != 6 ; ++i) field.next();
-
-    return field.count_occupied();
-}
+using namespace vector_implementation;
 
 /// 3D / 4D game of life
 Output day17(const std::vector<std::string> & lines, const DayExtraInfo &) {
