3330b3f4867e8d9f3406c93ebbede8b3ccd3cb84
Julian Bruyat <bruyat.ju@live.fr>
2020-12-20 08:37:59 +0100

    Day20 - Example works

--- src/day20.cpp
+++ src/day20.cpp
@@ -8,6 +8,7 @@
 #include <map>
 #include <regex>
 #include <ostream>
+#include <cmath>
 
 // https://adventofcode.com/2020/day/20
 
@@ -64,6 +65,9 @@ struct Path {
     }
 };
 
+using Paths = std::vector<Path>;
+using AllPaths = std::map<int, std::map<int, Paths>>;
+
 struct AltereredTile {
     Tile representation;
     Transformations transformations;
@@ -108,6 +112,30 @@ struct AltereredTile {
         stream << tile.representation << tile.transformations << "\n\n";
         return stream;
     }
+
+    [[nodiscard]] bool adjacent_left_to_right(const AltereredTile & other_tile) const;
+    [[nodiscard]] bool adjacent_top_to_down(const AltereredTile & other_tile) const;
+};
+
+struct TileStateExplorer {
+    int i = 0;
+
+    operator bool() {
+        return i != 2 * 2 * 4;
+    }
+
+    void operator()(AltereredTile & tile) {
+        tile.rotate_right();
+
+        if (i % 4 == 0) {
+            tile.flip_left_right();
+        }
+        if (i % 8 == 0) {
+            tile.flip_bottom_up();
+        }
+
+        ++i;
+    }
 };
 
 
@@ -121,39 +149,39 @@ static bool connect(const std::vector<std::vector<char>> & lhs, const std::vecto
     return true;
 }
 
+
+bool AltereredTile::adjacent_left_to_right(const AltereredTile & other_tile) const {
+    return connect(representation.image, other_tile.representation.image);
+}
+
+bool AltereredTile::adjacent_top_to_down(const AltereredTile & other_tile) const {
+    return representation.image.back() == other_tile.representation.image.front();
+}
+
+
 std::vector<Path> Tile::adjacent(const Tile & other_tile) const {
     std::vector<Path> paths;
 
     AltereredTile me { *this };
 
     std::set<Raw> my_raws;
-    std::set<Raw> other_raws;
 
     for (size_t i = 0 ; i != 4 ; ++i, me.rotate_right()) {
-        AltereredTile other { other_tile };
-
         if (my_raws.contains(me.representation.image)) {
             continue;
         }
 
         my_raws.insert(me.representation.image);
 
-        for (size_t j = 0 ; j != 2 ; ++j) {
-            other.flip_bottom_up();
+        std::set<Raw> other_raws;
+        AltereredTile other { other_tile };
 
-            for (size_t jj = 0 ; jj != 2 ; ++jj) {
-                other.flip_left_right();
+        for (TileStateExplorer trans ; trans ; trans(other)) {
+            if (!other_raws.contains(other.representation.image)) {
+                other_raws.insert(other.representation.image);
 
-                for (size_t jjj = 0 ; jjj != 4 ; ++jjj) {
-                    other.rotate_right();
-
-                    if (!other_raws.contains(other.representation.image)) {
-                        other_raws.insert(other.representation.image);
-
-                        if (connect(me.representation.image, other.representation.image)) {
-                            paths.push_back(Path { me.transformations.is_rotate_right, other.transformations });
-                        }
-                    }
+                if (connect(me.representation.image, other.representation.image)) {
+                    paths.push_back(Path { me.transformations.is_rotate_right, other.transformations });
                 }
             }
         }
@@ -185,20 +213,126 @@ std::vector<Tile> to_tiles(const std::vector<std::string> & lines) {
 }
 
 
+struct TempGrid {
+    std::vector<std::vector<std::optional<AltereredTile>>> m_grid;
+    const std::vector<Tile> & tiles;
+    const AllPaths & all_paths;
+    size_t m_size;
+
+    explicit TempGrid(const std::vector<Tile> & tiles, const AllPaths & all_paths) : tiles(tiles), all_paths(all_paths) {
+        const size_t size = static_cast<size_t>(std::sqrt(tiles.size()));
+
+        for (size_t i = 0 ; i != size; ++i) {
+            m_grid.emplace_back();
+            for (size_t j = 0 ; j != size ; ++j) {
+                m_grid.back().emplace_back(std::nullopt);
+            }
+        }
+        m_size = size;
+    }
+
+    bool resolve() {
+        std::set<int> used_tiles;
+        return try_place_at(0, 0, used_tiles);
+    }
+
+    bool try_place_at(size_t x, size_t y, std::set<int> & used_tiles) {
+        if (y == m_size) return true;
+
+        const size_t next_x = x != m_size - 1 ? x + 1 : 0;
+        const size_t next_y = x != m_size - 1 ?     y : y + 1;
+
+        const auto no_hope = [&](int me, int adjacent) {
+            const auto it = all_paths.find(adjacent);
+            const auto it2 = it->second.find(me);
+
+            return it2 == it->second.end();
+        };
+
+        for (const Tile & tile : tiles) {
+            if (used_tiles.contains(tile.id)) continue;
+
+            //if (x != 0 && m_grid[y][x - 1] && no_hope(tile.id, m_grid[y][x - 1]->representation.id)) continue;
+            //if (y != 0 && m_grid[y - 1][x] && no_hope(tile.id, m_grid[y - 1][x]->representation.id)) continue;
+
+            used_tiles.insert(tile.id);
+
+            m_grid[y][x] = AltereredTile { tile };
+
+
+            for (TileStateExplorer trans ; trans ; trans(*m_grid[y][x])) {
+                if (x != 0 && !m_grid[y][x - 1]->adjacent_left_to_right(*m_grid[y][x])) {
+                    continue;
+                }
+
+                if (y != 0 && !m_grid[y - 1][x]->adjacent_top_to_down(*m_grid[y][x])) {
+                    continue;
+                }
+
+                if (try_place_at(next_x, next_y, used_tiles)) {
+                    return true;
+                }
+            }
+
+            used_tiles.erase(tile.id);
+        }
+
+        return false;
+    }
+};
+
+struct Grid {
+    std::vector<std::vector<int>> m_grid;
+
+    explicit Grid(const TempGrid & temp_grid) {
+        for (const auto & line : temp_grid.m_grid) {
+            m_grid.emplace_back();
+            for (const auto & value : line) {
+                m_grid.back().emplace_back(value->representation.id);
+            }
+        }
+    }
+
+    [[nodiscard]] test::Value corners() const noexcept {
+        return test::Value(m_grid.front().front())
+            * test::Value(m_grid.front().back())
+            * test::Value(m_grid.back().front())
+            * test::Value(m_grid.back().back());
+    }
+
+
+    static std::optional<Grid> make(const std::vector<Tile> & tiles, const AllPaths & all_paths) {
+        TempGrid temp_grid { tiles, all_paths };
+
+        if (temp_grid.resolve()) {
+            return Grid(temp_grid);
+        } else {
+            return std::nullopt;
+        }
+    }
+};
 
 Output day20(const std::vector<std::string> & lines, const DayExtraInfo &) {
     std::vector<Tile> tiles = to_tiles(lines);
 
-    std::cout << tiles[0];
-    std::cout << tiles[1];
+    std::map<int, std::map<int, Paths>> all_paths;
 
-
-    const auto paths = tiles[0].adjacent(tiles[1]);
-
-    for (const auto & path : paths) {
-        std::cout << path << "\n";
+    for (const auto & tile : tiles) {
+        for (const auto & tile_ : tiles) {
+            if (&tile != &tile_) {
+                const auto paths = tile.adjacent(tile_);
+                if (!paths.empty()) {
+                    all_paths[tile.id][tile_.id] = paths;
+                }
+            }
+        }
     }
 
-    
-    return Output(0, 0);
+    std::optional<Grid> grid = Grid::make(tiles, all_paths);
+
+    if (grid) {
+        return Output(grid->corners(), 0);
+    } else {
+        return Output(0, 0);
+    }
 }
