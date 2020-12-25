f3dc3f7869efd6670ba04a49277ba5549f04741b
Julian Bruyat <bruyat.ju@live.fr>
2020-12-25 14:51:19 +0100

    Day 2020 01 - Part B doesn't search in already evaluated values (thx @BlueWorldInc for idea)

--- src/2020/day01_SumOfNumbers.cpp
+++ src/2020/day01_SumOfNumbers.cpp
@@ -5,7 +5,9 @@
 
 // https://adventofcode.com/2020/day/1
 
-static std::array<int, 2> find_sum_two(int target, const std::vector<int> & values) {
+// find_sum_two_set and find_sum_two_sorted have the same behavior
+
+static std::array<int, 2> find_sum_two_set(int target, const std::vector<int> & values) {
     std::unordered_set<int> expected_values;
 
     for (int value : values) {
@@ -19,16 +21,15 @@ static std::array<int, 2> find_sum_two(int target, const std::vector<int> & valu
     return { -1, -1 };
 }
 
-static std::optional<std::pair<int, int>> find_sum_three_values_(
+static std::optional<std::pair<int, int>> find_sum_two_sorted(
     const int target,
     std::vector<int>::iterator left,
-    std::vector<int>::iterator right,
-    std::vector<int>::iterator forbidden
+    std::vector<int>::iterator right
 ) {
     --right;
 
     while (true) {
-        if (forbidden == left || forbidden == right) {
+        if (left == right) {
             return std::nullopt;
         }
 
@@ -49,12 +50,12 @@ static std::array<int, 3> find_sum_three_values(
     std::vector<int>::iterator left,
     std::vector<int>::iterator right
 ) {
-    for (auto middle = left ; middle != right ; ++middle) {
-        auto f = find_sum_three_values_(target - *middle, left, right, middle);
-
-        if (f) {
-            return std::array<int, 3>{f->first, f->second, *middle};
+    while (left != right) {
+        if (const auto f = find_sum_two_sorted(target - *left, left + 1, right)) {
+            return std::array<int, 3>{f->first, f->second, *left};
         }
+
+        ++left;
     }
 
     return { -1, -1, -1 };
@@ -63,7 +64,7 @@ static std::array<int, 3> find_sum_three_values(
 Output day_2020_01(const std::vector<std::string> & lines, const DayExtraInfo &) {
     std::vector<int> numbers = lines_transform::to_ints(lines);
     
-    const auto r1 = find_sum_two(2020, numbers);
+    const auto r1 = find_sum_two_set(2020, numbers);
 
     std::sort(numbers.begin(), numbers.end());
     const auto r2 = find_sum_three_values(2020, numbers.begin(), numbers.end());
