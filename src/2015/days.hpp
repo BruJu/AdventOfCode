9bb7cffcd049bcf6f7e0f1195109763ea47785bd
Julian Bruyat <bruyat.ju@live.fr>
2020-12-27 13:52:34 +0100

    Day 2015 11

--- src/2015/days.hpp
+++ src/2015/days.hpp
@@ -10,9 +10,10 @@ Output day_2015_07(const std::vector<std::string> & lines, const DayExtraInfo &)
 Output day_2015_08(const std::vector<std::string> & lines, const DayExtraInfo &);
 Output day_2015_09(const std::vector<std::string> & lines, const DayExtraInfo &);
 Output day_2015_10(const std::vector<std::string> & lines, const DayExtraInfo &);
+Output day_2015_11(const std::vector<std::string> & lines, const DayExtraInfo &);
 
 static constexpr std::array<DayEntryPoint *, 25> days_2015 {
     &day_2015_01, &day_2015_02, &day_2015_03, nullptr, &day_2015_05,
     &day_2015_06, &day_2015_07, &day_2015_08, &day_2015_09, &day_2015_10,
-    nullptr
+    &day_2015_11, nullptr
 };
