95a5ee97dc0d8fbb6d4d9894b1cab60852a1a055
Julian Bruyat <bruyat.ju@live.fr>
2021-05-07 09:08:37 +0200

    Day 2016 2

--- src/2016/days.hpp
+++ src/2016/days.hpp
@@ -1,8 +1,9 @@
 #include "../framework/configuration.h"
 
 Output day_2016_01(const std::vector<std::string> & lines, const DayExtraInfo &);
+Output day_2016_02(const std::vector<std::string> & lines, const DayExtraInfo &);
 
 static constexpr std::array<DayEntryPoint *, 25> days_2016 {
-    &day_2016_01,
+    &day_2016_01, &day_2016_02,
     nullptr
 };
