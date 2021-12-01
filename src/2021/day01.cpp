509246c78adc2eebaa833fe62061041d22361909
Julian Bruyat <bruyat.ju@live.fr>
2021-12-01 16:59:09 +0100

    2021 - Day 1 - The gliding window is actually sliding

--- src/2021/day01.cpp
+++ src/2021/day01.cpp
@@ -13,12 +13,12 @@ Output day_2021_01(const std::vector<std::string> & lines, const DayExtraInfo &)
   }
 
   // Part B
-  static constexpr size_t gliding_window = 3;
+  static constexpr size_t sliding_window = 3;
   size_t part_b = 0;
-  int current_sum = std::reduce(numbers.begin(), numbers.begin() + gliding_window);
+  int current_sum = std::reduce(numbers.begin(), numbers.begin() + sliding_window);
 
-  for (auto it = numbers.begin() + gliding_window; it != numbers.end(); ++it) {
-    int new_sum = current_sum - *(it - gliding_window) + *it;
+  for (auto it = numbers.begin() + sliding_window; it != numbers.end(); ++it) {
+    int new_sum = current_sum - *(it - sliding_window) + *it;
     if (new_sum > current_sum) {
       ++part_b;
     }
