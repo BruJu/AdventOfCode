1273a432b8988347e26d33b700085480012559fb
Julian Bruyat <bruyat.ju@live.fr>
2021-12-14 06:06:08 +0100

    2021 14 boilerplate

---
+++ src/2021/day14.cpp
@@ -0,0 +1,29 @@
+#include "../advent_of_code.hpp"
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <map>
+#include "../util/position.hpp"
+
+// https://adventofcode.com/2021/day/14
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+Output day_2021_14(const std::vector<std::string> & lines, const DayExtraInfo & dei) {
+  std::string template_ = lines[0];
+  
+  for (auto it = lines.begin() + 2; it != lines.end(); ++it) {
+
+  }
+
+
+  for (int step = 0; step != 10; ++step) {
+    
+  }
+
+
+  // Count most freqent - less frequent
+
+  return Output(0, 0);
+}
