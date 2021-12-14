442a2968432c42c5750a5a7247b041ab53e99258
BruJu <bruyat.ju@live.fr>
2021-12-14 07:14:32 +0100

    2021 14 ok

--- src/2021/day14.cpp
+++ src/2021/day14.cpp
@@ -8,17 +8,22 @@
 
 // https://adventofcode.com/2021/day/14
 
-///////////////////////////////////////////////////////////////////////////////
 
-using Sequence = std::pair<char, char>;
-using FlatSequence = std::map<Sequence, long long int>;
-using Transformations = std::map<Sequence, std::array<Sequence, 2>>;
+/** A letter and the next one */
+using Domino = std::pair<char, char>;
 
-static FlatSequence next_step(const FlatSequence & current, const Transformations & transformations) {
-  FlatSequence next;
+/** Number of occurrences of a domino */
+using Dominos = std::map<Domino, long long int>;
 
-  for (const auto & [sequence, times] : current) {
-    const auto [a, b] = transformations.find(sequence)->second;
+/** The two Domino to create for each Domino */
+using Transformations = std::map<Domino, std::array<Domino, 2>>;
+
+/** Process a transformation */
+static Dominos next_step(const Dominos & current, const Transformations & transformations) {
+  Dominos next;
+
+  for (const auto & [domino, times] : current) {
+    const auto [a, b] = transformations.find(domino)->second;
 
     next[a] += times;
     next[b] += times;
@@ -27,20 +32,17 @@ static FlatSequence next_step(const FlatSequence & current, const Transformation
   return next;
 }
 
-static std::map<char, long long int> count_occurrences(const FlatSequence & flat_seq) {
-  std::map<char, long long int> count;
-
+/** Return the differrence between the max nb of occurrences of a letter and the min */
+static long long int answer(char first_letter, const Dominos & flat_seq) {
+  // == Count number of occurrences of each letter.
+  // First letter -> Not on the right side of any domino
+  std::map<char, long long int> count { { first_letter, 1 } };
+  // Count right side of all dominos
   for (const auto & [sequence, times] : flat_seq) {
-    count[sequence.first] += times;
     count[sequence.second] += times;
   }
 
-  return count;
-}
-
-long long int answer(const FlatSequence & flat_seq) {
-  const auto count = count_occurrences(flat_seq);
-
+  // == Look for min and max
   std::optional<long long int> min;
   std::optional<long long int> max;
 
@@ -52,43 +54,37 @@ long long int answer(const FlatSequence & flat_seq) {
   return *max - *min;
 }
 
-long long int length(const FlatSequence & flat_seq) {
-  long long int c = 1;
-
-  for (const auto & [_, cnt] : flat_seq) {
-    c += cnt;
-  }
-
-  return c;
-}
-
-
-Output day_2021_14(const std::vector<std::string> & lines, const DayExtraInfo & dei) {
-  FlatSequence current;
+Output day_2021_14(const std::vector<std::string> & lines, const DayExtraInfo &) {
+  // == Transform the input into a list of dominos
+  Dominos current;
 
   std::string template_ = lines[0];
   for (size_t i = 0; i != template_.size() - 1; ++i) {
-    current[Sequence{ template_[i], template_[i + 1] }] += 1;
+    current[Domino{ template_[i], template_[i + 1] }] += 1;
   }
 
+  // == Read the transformations
   Transformations transformations;
   for (auto it = lines.begin() + 2; it != lines.end(); ++it) {
     const auto & this_trans = *it;
-    transformations[Sequence{ this_trans[0], this_trans[1] }] = {
-      Sequence{ this_trans[0], this_trans[6] },
-      Sequence{ this_trans[6], this_trans[1] }
+    transformations[Domino{ this_trans[0], this_trans[1] }] = {
+      Domino{ this_trans[0], this_trans[6] },
+      Domino{ this_trans[6], this_trans[1] }
     };
   }
-    std::cout << answer(current) << ' ' << length(current) << '\n';
 
+  // == Part A
   for (int step = 0; step != 10; ++step) {
     current = next_step(current, transformations);
-    std::cout << answer(current) << " length=" << length(current) <<'\n';
   }
+  const long long int part_a = answer(template_[0], current);
 
-  // Count most frequent - less frequent
+  // == Part B
+  for (int step = 10; step != 40; ++step) {
+    current = next_step(current, transformations);
+  }
+  const long long int part_b = answer(template_[0], current);
 
-  long long int part_a = answer(current);
-
-  return Output(part_a, 0);
+  // Ok
+  return Output(part_a, part_b);
 }
