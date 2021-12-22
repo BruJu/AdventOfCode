02aa7836fcfd9f3fe142de1c3f7a8f86019a3e57
Julian Bruyat <bruyat.ju@live.fr>
2021-12-22 22:36:08 +0100

    2021 22 tried to split on the fly, it doesn't work or it's too slow

--- src/2021/day22.cpp
+++ src/2021/day22.cpp
@@ -2,48 +2,131 @@
 #include <regex>
 #include <vector>
 #include <unordered_map>
+#include <map>
+#include <set>
 #include <tuple>
+#include <iostream>
 
-struct Cube {
-  int x_min;
-  int x_max;
-  int y_min;
-  int y_max;
-  int z_min;
-  int z_max;
+static constexpr size_t X = 0;
+static constexpr size_t Y = 1;
+static constexpr size_t Z = 2;
 
-  [[nodiscard]] bool is_init_protocole() const {
-    return x_min >= -50 && x_max <= 50
-      && y_min >= -50 && y_max <= 50
-      && z_min >= -50 && z_max <= 50;
+struct Segment {
+  int min;
+  int max;
+
+  [[nodiscard]] bool is_init_protocole() const noexcept {
+    return min >= -50 && max <= 50;
   }
 
-  [[nodiscard]] long long int volume() const {
-    const long long int width = x_max - x_min + 1;
-    const long long int height = y_max - y_min + 1;
-    const long long int depth = z_max - z_min + 1;
-    return width * height * depth;
+  [[nodiscard]] long long int nb_of_points() const noexcept {
+    return max - min + 1;
   }
 
-  [[nodiscard]] bool equals_or_disjoint_to(const Cube other) const {
-    if (x_min == other.x_min && x_max == other.x_max
-      && y_min == other.y_min && y_max == other.y_max
-      && z_min == other.z_min && z_max == other.z_max) {
-      return true;
-    }
+  [[nodiscard]] bool operator==(const Segment other) const noexcept {
+    return min == other.min && max == other.max;
+  }
 
-    if (x_min > other.x_max) return true;
-    if (other.x_min > x_max) return true;
-    if (y_min > other.y_max) return true;
-    if (other.y_min > y_max) return true;
-    if (z_min > other.z_max) return true;
-    if (other.z_min > z_max) return true;
+  [[nodiscard]] bool disjoint_to(const Segment other) const noexcept {
+    return /* This is after */ min > other.max || /* Other is before */ max < other.min;
+  }
 
+  [[nodiscard]] bool operator<(const Segment other) const noexcept {
+    if (min < other.min) return true;
+    if (min > other.min) return false;
+    if (max < other.max) return true;
+    if (max > other.max) return false;
     return false;
   }
 
-  void split(const Cube & cube, std::vector<Cube> & destination) {
-    // TODO
+  friend std::ostream & operator<<(std::ostream & stream, Segment seg) {
+    return stream << seg.min << "~" << seg.max;
+  }
+};
+
+struct Cube {
+  std::array<Segment, 3> xyz;
+
+  Cube(int x_min, int x_max, int y_min, int y_max, int z_min, int z_max) {
+    xyz[X] = Segment { x_min, x_max };
+    xyz[Y] = Segment { y_min, y_max };
+    xyz[Z] = Segment { z_min, z_max };
+  }
+
+  friend std::ostream & operator<<(std::ostream & stream, Cube cube) {
+    return stream << "(" << cube.xyz[X] << " | " << cube.xyz[Y] << " | " << cube.xyz[Z] << ")";
+  }
+
+  [[nodiscard]] bool is_init_protocole() const {
+    return xyz[X].is_init_protocole()
+      && xyz[Y].is_init_protocole()
+      && xyz[Z].is_init_protocole();
+  }
+
+  [[nodiscard]] long long int volume() const {
+    return xyz[X].nb_of_points() * xyz[Y].nb_of_points() * xyz[Z].nb_of_points();
+  }
+
+  [[nodiscard]] bool operator==(const Cube & other) const {
+    return xyz == other.xyz;
+  }
+
+  [[nodiscard]] bool operator<(const Cube & other) const {
+    if (xyz[X] < other.xyz[X]) return true;
+    if (other.xyz[X] < xyz[X]) return false;
+    if (xyz[Y] < other.xyz[Y]) return true;
+    if (other.xyz[Y] < xyz[Y]) return false;
+    if (xyz[Z] < other.xyz[Z]) return true;
+    if (other.xyz[Z] < xyz[Z]) return false;
+    return false;
+  }
+
+  [[nodiscard]] bool disjoint_to(const Cube & other) const {
+    return xyz[X].disjoint_to(other.xyz[X])
+      || xyz[Y].disjoint_to(other.xyz[Y])
+      || xyz[Z].disjoint_to(other.xyz[Z]);
+  }
+
+  [[nodiscard]] bool equals_or_disjoint_to(const Cube other) const {
+    return *this == other || disjoint_to(other);
+  }
+
+  static std::vector<Cube> ensure_distinct(const Cube lhs, const Cube rhs) {
+    if (lhs.equals_or_disjoint_to(rhs)) return {};
+
+    std::vector<Cube> cut;
+
+    std::vector<int> xs = { lhs.xyz[X].min, lhs.xyz[X].max, rhs.xyz[X].min, rhs.xyz[X].max };
+    std::vector<int> ys = { lhs.xyz[Y].min, lhs.xyz[Y].max, rhs.xyz[Y].min, rhs.xyz[Y].max };
+    std::vector<int> zs = { lhs.xyz[Z].min, lhs.xyz[Z].max, rhs.xyz[Z].min, rhs.xyz[Z].max };
+
+    constexpr auto clear_vector = [](std::vector<int> & vec) {
+      std::sort(vec.begin(), vec.end());
+      const auto last = std::unique(vec.begin(), vec.end());
+      vec.erase(last, vec.end());
+    };
+    
+    clear_vector(xs);
+    clear_vector(ys);
+    clear_vector(zs);
+
+    for (size_t xi = 0; xi + 1 != xs.size(); ++xi) {
+      for (size_t yi = 0; yi + 1 != ys.size(); ++yi) {
+        for (size_t zi = 0; zi + 1 != zs.size(); ++zi) {
+          Cube cube(
+            xs[xi], xs[xi + 1] - 1,
+            ys[yi], ys[yi + 1] - 1,
+            zs[zi], zs[zi + 1] - 1
+          );
+
+          if (!lhs.disjoint_to(cube) || !rhs.disjoint_to(cube)) {
+            cut.push_back(cube);
+          }
+        }
+      }
+    }
+
+    return cut;
   }
 };
 
@@ -71,15 +154,12 @@ struct PartA {
 
   void run(const Instruction & instruction) {
     if (instruction.cube.is_init_protocole()) {
-      run(instruction.add, instruction.cube);
-    }
-  }
-
-  void run(bool new_state, Cube cube) {
-    for (int x = cube.x_min; x <= cube.x_max; ++x) {
-      for (int y = cube.y_min; y <= cube.y_max; ++y) {
-        for (int z = cube.z_min; z <= cube.z_max; ++z) {
-          area[x + 50][y + 50][z + 50] = new_state;
+      const auto & cube = instruction.cube.xyz;
+      for (int x = cube[X].min; x <= cube[X].max; ++x) {
+        for (int y = cube[Y].min; y <= cube[Y].max; ++y) {
+          for (int z = cube[Z].min; z <= cube[Z].max; ++z) {
+            area[x + 50][y + 50][z + 50] = instruction.add;
+          }
         }
       }
     }
@@ -98,17 +178,76 @@ struct PartA {
 };
 
 struct PartB {
-  std::vector<Cube> lit_cubes;
+  std::map<Cube, std::vector<Cube>> managed_cubes = {};
+  std::set<Cube> state = {};
+
+  explicit PartB(std::vector<Cube> cubes) {
+    const auto original = cubes;
+    bool stable = false;
+    while (!stable) {
+      // std::cout << cubes.size() << '\n';
+      stable = true;
+
+      for (size_t i_lhs = 0; i_lhs + 1 < cubes.size(); ++i_lhs) {
+        // std::cout << "lhs=" << i_lhs << '\n';
+
+        size_t i_rhs = i_lhs + 1;
+
+        while (i_rhs < cubes.size()) {
+          // std::cout << "rhs=" << i_rhs << '\n';
+          const auto cut = Cube::ensure_distinct(cubes[i_lhs], cubes[i_rhs]);
+
+          if (cut.size() != 0) {
+            if (cut.size() < 2)  {
+              // std::cout << cubes[i_lhs] << " & " << cubes[i_rhs] << " made one \n";
+            } else if (cut.size() == 2) {
+              // std::cout << cubes[i_lhs] << " & " << cubes[i_rhs] << " not equaldistinct but two blocks\n";
+            } else {
+              // std::cout << cubes[i_lhs] << " & " << cubes[i_rhs] << " boom\n";
+              cubes[i_lhs] = cut[0];
+              cubes[i_rhs] = cut[1];
+              for (size_t i = 2; i != cut.size(); ++i) {
+                cubes.push_back(cut[i]);
+              }
+            }
+          }
+
+          ++i_rhs;
+        }
+      }
+    }
+
+    std::cout << "end\n";
+
+    for (const auto cube : cubes) {
+      for (const auto ori : original) {
+        if (!ori.disjoint_to(cube)) {
+          managed_cubes[ori].push_back(cube);
+        }
+      }
+    }
+
+    std::cout << "true end\n";
+  }
 
   void run(const Instruction & instruction) {
+    const auto & cubes = managed_cubes.find(instruction.cube)->second;
 
+    for (const auto cube : cubes) {
+      if (instruction.add) {
+        state.emplace(cube);
+      } else {
+        const auto it = state.find(cube);
+        if (it != state.end()) state.erase(it);
+      }
+    }
   }
 
 
   [[nodiscard]] long long int how_many_lit() const {
     long long int total = 0;
 
-    for (const auto & cube : lit_cubes) {
+    for (const auto & cube : state) {
       total += cube.volume();
     }
 
@@ -131,14 +270,14 @@ Output day_2021_22(const std::vector<std::string> & lines, const DayExtraInfo &)
       continue;
     }
 
-    Cube cube {
+    Cube cube = Cube(
       std::stoi(matches[2].str()),
       std::stoi(matches[3].str()),
       std::stoi(matches[4].str()),
       std::stoi(matches[5].str()),
       std::stoi(matches[6].str()),
-      std::stoi(matches[7].str()),
-    };
+      std::stoi(matches[7].str())
+    );
 
     instructions.push_back(Instruction{ matches[1] == "on", { cube } });
   }
@@ -147,6 +286,18 @@ Output day_2021_22(const std::vector<std::string> & lines, const DayExtraInfo &)
   PartA slow_part_a;
   for (const auto & instruction : instructions) {
     slow_part_a.run(instruction);
+  }
+
+std::cout << "B\n";
+  // Part B
+  std::vector<Cube> cubes;
+  for (const auto & instruction : instructions) {
+    cubes.push_back(instruction.cube);
+  }
+std::cout << "ok\n";
+  PartB part_b(cubes);
+  for (const auto & instruction : instructions) {
+    part_b.run(instruction);
   }
 
   return Output(slow_part_a.how_many_lit(), 0);
