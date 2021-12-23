e386241c40e3789e94cfee5e1c8dff71894be132
Julian Bruyat <bruyat.ju@live.fr>
2021-12-23 01:01:08 +0100

    2021 22 changed approach still doesnt work

--- src/2021/day22.cpp
+++ src/2021/day22.cpp
@@ -7,84 +7,83 @@
 #include <tuple>
 #include <iostream>
 
-static constexpr size_t X = 0;
-static constexpr size_t Y = 1;
-static constexpr size_t Z = 2;
-
 struct Segment {
   int min;
-  int max;
+  int max_ex;
 
   [[nodiscard]] bool is_init_protocole() const noexcept {
-    return min >= -50 && max <= 50;
+    return min >= -50 && max_ex < 51;
   }
 
   [[nodiscard]] long long int nb_of_points() const noexcept {
-    return max - min + 1;
+    return max_ex - min;
   }
 
   [[nodiscard]] bool operator==(const Segment other) const noexcept {
-    return min == other.min && max == other.max;
+    return min == other.min && max_ex == other.max_ex;
   }
 
   [[nodiscard]] bool disjoint_to(const Segment other) const noexcept {
-    return /* This is after */ min > other.max || /* Other is before */ max < other.min;
+    return /* This is after */ min >= other.max_ex
+      || /* Other is before */ other.min >= max_ex;
   }
 
   [[nodiscard]] bool operator<(const Segment other) const noexcept {
     if (min < other.min) return true;
     if (min > other.min) return false;
-    if (max < other.max) return true;
-    if (max > other.max) return false;
+    if (max_ex < other.max_ex) return true;
+    if (max_ex > other.max_ex) return false;
     return false;
   }
 
   friend std::ostream & operator<<(std::ostream & stream, Segment seg) {
-    return stream << seg.min << "~" << seg.max;
+    return stream << seg.min << "~" << (seg.max_ex);
   }
 };
 
 struct Cube {
-  std::array<Segment, 3> xyz;
+  Segment x;
+  Segment y;
+  Segment z;
 
   Cube(int x_min, int x_max, int y_min, int y_max, int z_min, int z_max) {
-    xyz[X] = Segment { x_min, x_max };
-    xyz[Y] = Segment { y_min, y_max };
-    xyz[Z] = Segment { z_min, z_max };
+    x = Segment{ x_min, x_max + 1 };
+    y = Segment{ y_min, y_max + 1 };
+    z = Segment{ z_min, z_max + 1 };
   }
 
   friend std::ostream & operator<<(std::ostream & stream, Cube cube) {
-    return stream << "(" << cube.xyz[X] << " | " << cube.xyz[Y] << " | " << cube.xyz[Z] << ")";
+    return stream << "(" << cube.x << " | " << cube.y << " | " << cube.z << ")";
   }
 
   [[nodiscard]] bool is_init_protocole() const {
-    return xyz[X].is_init_protocole()
-      && xyz[Y].is_init_protocole()
-      && xyz[Z].is_init_protocole();
+    return x.is_init_protocole()
+      && y.is_init_protocole()
+      && z.is_init_protocole();
   }
 
   [[nodiscard]] long long int volume() const {
-    return xyz[X].nb_of_points() * xyz[Y].nb_of_points() * xyz[Z].nb_of_points();
+    return x.nb_of_points() * y.nb_of_points() * z.nb_of_points();
   }
 
   [[nodiscard]] bool operator==(const Cube & other) const {
-    return xyz == other.xyz;
+    return x == other.x && y == other.y && z == other.z;
   }
 
   [[nodiscard]] bool operator<(const Cube & other) const {
-    if (xyz[X] < other.xyz[X]) return true;
-    if (other.xyz[X] < xyz[X]) return false;
-    if (xyz[Y] < other.xyz[Y]) return true;
-    if (other.xyz[Y] < xyz[Y]) return false;
-    if (xyz[Z] < other.xyz[Z]) return true;
-    if (other.xyz[Z] < xyz[Z]) return false;
+    if (x < other.x) return true;
+    if (other.x < x) return false;
+    if (y < other.y) return true;
+    if (other.y < y) return false;
+    if (z < other.z) return true;
+    if (other.z < z) return false;
     return false;
   }
 
   [[nodiscard]] bool disjoint_to(const Cube & other) const {
-    return xyz[X].disjoint_to(other.xyz[X])
-      || xyz[Y].disjoint_to(other.xyz[Y])
-      || xyz[Z].disjoint_to(other.xyz[Z]);
+    return x.disjoint_to(other.x)
+      || y.disjoint_to(other.y)
+      || z.disjoint_to(other.z);
   }
 
   [[nodiscard]] bool equals_or_disjoint_to(const Cube other) const {
@@ -92,18 +91,20 @@ struct Cube {
   }
 
   static std::vector<Cube> ensure_distinct(const Cube lhs, const Cube rhs) {
-    if (lhs.equals_or_disjoint_to(rhs)) return {};
-
     std::vector<Cube> cut;
 
-    std::vector<int> xs = { lhs.xyz[X].min, lhs.xyz[X].max, rhs.xyz[X].min, rhs.xyz[X].max };
-    std::vector<int> ys = { lhs.xyz[Y].min, lhs.xyz[Y].max, rhs.xyz[Y].min, rhs.xyz[Y].max };
-    std::vector<int> zs = { lhs.xyz[Z].min, lhs.xyz[Z].max, rhs.xyz[Z].min, rhs.xyz[Z].max };
+    if (lhs.equals_or_disjoint_to(rhs)) return cut;
+
+    std::vector<int> xs = { lhs.x.min, lhs.x.max_ex, rhs.x.min, rhs.x.max_ex };
+    std::vector<int> ys = { lhs.y.min, lhs.y.max_ex, rhs.y.min, rhs.y.max_ex };
+    std::vector<int> zs = { lhs.z.min, lhs.z.max_ex, rhs.z.min, rhs.z.max_ex };
 
     constexpr auto clear_vector = [](std::vector<int> & vec) {
       std::sort(vec.begin(), vec.end());
       const auto last = std::unique(vec.begin(), vec.end());
       vec.erase(last, vec.end());
+
+      //std::cout << vec[0] << "," << vec[1]<<','<<vec[2]<<'\n';
     };
     
     clear_vector(xs);
@@ -154,10 +155,10 @@ struct PartA {
 
   void run(const Instruction & instruction) {
     if (instruction.cube.is_init_protocole()) {
-      const auto & cube = instruction.cube.xyz;
-      for (int x = cube[X].min; x <= cube[X].max; ++x) {
-        for (int y = cube[Y].min; y <= cube[Y].max; ++y) {
-          for (int z = cube[Z].min; z <= cube[Z].max; ++z) {
+      const auto & cube = instruction.cube;
+      for (int x = cube.x.min; x < cube.x.max_ex; ++x) {
+        for (int y = cube.y.min; y < cube.y.max_ex; ++y) {
+          for (int z = cube.z.min; z < cube.z.max_ex; ++z) {
             area[x + 50][y + 50][z + 50] = instruction.add;
           }
         }
@@ -185,39 +186,45 @@ struct PartB {
     const auto original = cubes;
     bool stable = false;
     while (!stable) {
-      // std::cout << cubes.size() << '\n';
+      std::cout << cubes.size() << '\n';
       stable = true;
 
       for (size_t i_lhs = 0; i_lhs + 1 < cubes.size(); ++i_lhs) {
         // std::cout << "lhs=" << i_lhs << '\n';
 
-        size_t i_rhs = i_lhs + 1;
-
-        while (i_rhs < cubes.size()) {
+        for (size_t i_rhs = i_lhs + 1; i_rhs < cubes.size(); ++i_rhs) {
           // std::cout << "rhs=" << i_rhs << '\n';
           const auto cut = Cube::ensure_distinct(cubes[i_lhs], cubes[i_rhs]);
 
           if (cut.size() != 0) {
             if (cut.size() < 2)  {
+              //std::cout << "bad fusion\n";
               // std::cout << cubes[i_lhs] << " & " << cubes[i_rhs] << " made one \n";
             } else if (cut.size() == 2) {
-              // std::cout << cubes[i_lhs] << " & " << cubes[i_rhs] << " not equaldistinct but two blocks\n";
+              //std::cout << "bad cut\n";
+              //std::cout << cubes[i_lhs] << " & " << cubes[i_rhs] << " not equaldistinct but two blocks\n";
+              //std::cout << cut[0] << " & " << cut[1] << " not equaldistinct but two blocks\n";
             } else {
-              // std::cout << cubes[i_lhs] << " & " << cubes[i_rhs] << " boom\n";
+              //std::cout << cubes[i_lhs] << " & " << cubes[i_rhs] << " boom\n";
               cubes[i_lhs] = cut[0];
               cubes[i_rhs] = cut[1];
               for (size_t i = 2; i != cut.size(); ++i) {
                 cubes.push_back(cut[i]);
               }
+              //std::cout << "+" << cut.size() - 2 << '\n';
+
+              stable = false;
             }
           }
-
-          ++i_rhs;
         }
       }
     }
 
     std::cout << "end\n";
+
+    //for (const auto cuber : cubes) {
+    //  std::cout << cuber << '\n';
+    //}
 
     for (const auto cube : cubes) {
       for (const auto ori : original) {
@@ -228,6 +235,8 @@ struct PartB {
     }
 
     std::cout << "true end\n";
+
+    std::cout << cubes.size() << '\n';
   }
 
   void run(const Instruction & instruction) {
@@ -238,7 +247,11 @@ struct PartB {
         state.emplace(cube);
       } else {
         const auto it = state.find(cube);
-        if (it != state.end()) state.erase(it);
+        if (it != state.end()) {
+          //std::cout << "erased" << how_many_lit() << '\n';
+          state.erase(it);
+          //std::cout << "post erased" << how_many_lit() << '\n';
+        }
       }
     }
   }
@@ -300,5 +313,5 @@ std::cout << "ok\n";
     part_b.run(instruction);
   }
 
-  return Output(slow_part_a.how_many_lit(), 0);
+  return Output(slow_part_a.how_many_lit(), part_b.how_many_lit());
 }
