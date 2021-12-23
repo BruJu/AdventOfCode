829ebd689280a6e4a9bad82e99a9086415a5b6a8
Julian Bruyat <bruyat.ju@live.fr>
2021-12-23 02:26:32 +0100

    2021 22 B

--- src/2021/day22.cpp
+++ src/2021/day22.cpp
@@ -4,148 +4,258 @@
 #include <unordered_map>
 #include <map>
 #include <set>
+#include <optional>
 #include <tuple>
 #include <iostream>
 
-struct Segment {
-  int min;
-  int max_ex;
+// https://adventofcode.com/2021/day/22
 
-  [[nodiscard]] bool is_init_protocole() const noexcept {
-    return min >= -50 && max_ex < 51;
+// For part B, I tried to split intersected cuboids but I couldn't get the maths
+// right. I found of Reddit another approach which relies on compression so
+// I've implemented instead.
+//
+// The idea is to have a grid of point, but each point in the virtual grid can
+// represent several points. 
+//
+// For example let us consider that we have those cuboids:
+//
+// 3
+// 2   bbbb
+// 1 aaxbbb
+// 0 aaa
+// . 0123456
+//
+// We build this virtual layout:
+//
+// 2   bbbb
+// 1 aaxbbb
+// 0 aaa
+// . 0 1  2
+//
+// A position on an axis in the virtual layout is named 
+
+
+struct VirtualCoordinate {
+  int position;
+  long long int absorbed;
+
+  VirtualCoordinate(int min, int max) : position(min), absorbed(max - min) {}
+};
+
+struct VirtualAxis {
+  std::vector<VirtualCoordinate> coordinates;
+
+  VirtualAxis(std::vector<int> values) {
+    // Sort + remove duplicates
+    std::sort(values.begin(), values.end());
+    const auto it = std::unique(values.begin(), values.end());
+    if (it != values.end()) {
+      values.erase(it);
+    }
+
+    // Build list of virtual coordinates
+    for (size_t i = 0; i + 1 < values.size(); ++i) {
+      coordinates.emplace_back(values[i], values[i + 1]);
+    }
+  }
+  
+  [[nodiscard]] auto size() const noexcept { return coordinates.size(); }
+
+  std::optional<std::pair<int, int>> find_virtual_segment(int min, int max) const {
+    int it = 0;
+
+    while (it != coordinates.size()) {
+      if (coordinates[it].position == min) break;
+      ++it;
+    }
+
+    if (it == coordinates.size()) {
+      return std::nullopt;
+    }
+
+    const int black_hole_starts_at = it;
+    
+    int quantity_to_absorb = max - min;
+    quantity_to_absorb -= coordinates[it].absorbed;
+
+    while (quantity_to_absorb > 0) {
+      ++it;
+      if (it == coordinates.size()) {
+        return std::nullopt;
+      }
+      quantity_to_absorb -= coordinates[it].absorbed;
+    }
+
+    if (quantity_to_absorb == 0) {
+      ++it;
+      return std::pair<int, int>(black_hole_starts_at, it);
+    } else {
+      return std::nullopt;
+    }
   }
 
-  [[nodiscard]] long long int nb_of_points() const noexcept {
-    return max_ex - min;
-  }
-
-  [[nodiscard]] bool operator==(const Segment other) const noexcept {
-    return min == other.min && max_ex == other.max_ex;
-  }
-
-  [[nodiscard]] bool disjoint_to(const Segment other) const noexcept {
-    return /* This is after */ min >= other.max_ex
-      || /* Other is before */ other.min >= max_ex;
-  }
-
-  [[nodiscard]] bool operator<(const Segment other) const noexcept {
-    if (min < other.min) return true;
-    if (min > other.min) return false;
-    if (max_ex < other.max_ex) return true;
-    if (max_ex > other.max_ex) return false;
-    return false;
-  }
-
-  friend std::ostream & operator<<(std::ostream & stream, Segment seg) {
-    return stream << seg.min << "~" << (seg.max_ex);
+  [[nodiscard]] long long int operator[](size_t i) const {
+    return coordinates[i].absorbed;
   }
 };
 
-struct Cube {
-  Segment x;
-  Segment y;
-  Segment z;
+/** A 3D position */
+struct Point3D {
+  int x;
+  int y;
+  int z;
+};
 
-  Cube(int x_min, int x_max, int y_min, int y_max, int z_min, int z_max) {
-    x = Segment{ x_min, x_max + 1 };
-    y = Segment{ y_min, y_max + 1 };
-    z = Segment{ z_min, z_max + 1 };
-  }
+/** A 3D area */
+struct Cuboid {
+  Point3D min;
+  Point3D max;
 
-  friend std::ostream & operator<<(std::ostream & stream, Cube cube) {
-    return stream << "(" << cube.x << " | " << cube.y << " | " << cube.z << ")";
-  }
-
-  [[nodiscard]] bool is_init_protocole() const {
-    return x.is_init_protocole()
-      && y.is_init_protocole()
-      && z.is_init_protocole();
-  }
-
-  [[nodiscard]] long long int volume() const {
-    return x.nb_of_points() * y.nb_of_points() * z.nb_of_points();
-  }
-
-  [[nodiscard]] bool operator==(const Cube & other) const {
-    return x == other.x && y == other.y && z == other.z;
-  }
-
-  [[nodiscard]] bool operator<(const Cube & other) const {
-    if (x < other.x) return true;
-    if (other.x < x) return false;
-    if (y < other.y) return true;
-    if (other.y < y) return false;
-    if (z < other.z) return true;
-    if (other.z < z) return false;
-    return false;
-  }
-
-  [[nodiscard]] bool disjoint_to(const Cube & other) const {
-    return x.disjoint_to(other.x)
-      || y.disjoint_to(other.y)
-      || z.disjoint_to(other.z);
-  }
-
-  [[nodiscard]] bool equals_or_disjoint_to(const Cube other) const {
-    return *this == other || disjoint_to(other);
-  }
-
-  static std::vector<Cube> ensure_distinct(const Cube lhs, const Cube rhs) {
-    std::vector<Cube> cut;
-
-    if (lhs.equals_or_disjoint_to(rhs)) return cut;
-
-    std::vector<int> xs = { lhs.x.min, lhs.x.max_ex, rhs.x.min, rhs.x.max_ex };
-    std::vector<int> ys = { lhs.y.min, lhs.y.max_ex, rhs.y.min, rhs.y.max_ex };
-    std::vector<int> zs = { lhs.z.min, lhs.z.max_ex, rhs.z.min, rhs.z.max_ex };
-
-    constexpr auto clear_vector = [](std::vector<int> & vec) {
-      std::sort(vec.begin(), vec.end());
-      const auto last = std::unique(vec.begin(), vec.end());
-      vec.erase(last, vec.end());
-
-      //std::cout << vec[0] << "," << vec[1]<<','<<vec[2]<<'\n';
-    };
-    
-    clear_vector(xs);
-    clear_vector(ys);
-    clear_vector(zs);
-
-    for (size_t xi = 0; xi + 1 != xs.size(); ++xi) {
-      for (size_t yi = 0; yi + 1 != ys.size(); ++yi) {
-        for (size_t zi = 0; zi + 1 != zs.size(); ++zi) {
-          Cube cube(
-            xs[xi], xs[xi + 1] - 1,
-            ys[yi], ys[yi + 1] - 1,
-            zs[zi], zs[zi + 1] - 1
-          );
-
-          if (!lhs.disjoint_to(cube) || !rhs.disjoint_to(cube)) {
-            cut.push_back(cube);
-          }
+  template<typename Consumer>
+  void for_each_point(Consumer consumer) const {
+    for (int x = min.x; x < max.x; ++x) {
+      for (int y = min.y; y < max.y; ++y) {
+        for (int z = min.z; z < max.z; ++z) {
+          consumer(Point3D{ x, y, z });
         }
       }
     }
-
-    return cut;
   }
 };
 
+/** An instruction to turn on or off the lights in an area*/
 struct Instruction {
   bool add;
-  Cube cube;
+  Cuboid cube;
 };
 
-struct Point3D : std::tuple<int, int, int> {};
+/** The entire universe, with a new compressed graduation for our use case */
+struct Universe {
+  VirtualAxis x;
+  VirtualAxis y;
+  VirtualAxis z;
+
+  static Universe build_from_instructions(
+    const std::vector<Instruction> & instructions
+  ) {
+    std::vector<int> xs;
+    std::vector<int> ys;
+    std::vector<int> zs;
+
+    for (const auto & instruction : instructions) {
+      xs.push_back(instruction.cube.min.x);
+      ys.push_back(instruction.cube.min.y);
+      zs.push_back(instruction.cube.min.z);
+      xs.push_back(instruction.cube.max.x);
+      ys.push_back(instruction.cube.max.y);
+      zs.push_back(instruction.cube.max.z);
+    }
+
+    return Universe(xs, ys, zs);
+  }
+
+  Universe(std::vector<int> xs, std::vector<int> ys, std::vector<int> zs)
+  : x(xs), y(ys), z(zs) {}
+
+  /** Remap an absolute cuboid to a virtual cuboid in this universe */
+  [[nodiscard]] std::optional<Cuboid> transform(Cuboid cube) const {
+    const auto xs = x.find_virtual_segment(cube.min.x, cube.max.x);
+    const auto ys = y.find_virtual_segment(cube.min.y, cube.max.y);
+    const auto zs = z.find_virtual_segment(cube.min.z, cube.max.z);
+
+    if (xs && ys && zs) {
+      return Cuboid {
+        Point3D { xs->first, ys->first, zs->first },
+        Point3D { xs->second, ys->second, zs->second }
+      };
+    } else {
+      return std::nullopt;
+    }
+  }
+
+  template <typename Consumer>
+  void travel_the_universe(Consumer consumer) const {
+    const Cuboid whole_area {
+      Point3D{ 0, 0, 0 },
+      Point3D{ x.size(), y.size(), z.size() }
+    };
+
+    whole_area.for_each_point(consumer);
+  }
+
+  [[nodiscard]] long long int operator[](Point3D point) const {
+    return x[point.x] * y[point.y] * z[point.z];
+  }
+};
+
+struct HitTheLights {
+  static long long int let_the_music_move_you(
+    const std::vector<Instruction> & instructions
+  ) {
+    HitTheLights light(Universe::build_from_instructions(instructions));
+    light.run(instructions);
+    return light.the_answer_is();
+  }
+
+  Universe universe;
+  std::vector<bool> blackstar;
+
+  HitTheLights(Universe universe)
+  : universe(universe),
+    blackstar(universe.x.size() * universe.y.size() * universe.z.size(), false)
+  {}
+
+  void run(const std::vector<Instruction> & instructions) {
+    for (Instruction instruction : instructions) {
+      const auto new_cube = universe.transform(instruction.cube);
+      if (!new_cube) {
+        std::cout << "Error on cube virtualization\n";
+        return;
+      }
+
+      new_cube->for_each_point([&](Point3D point) {
+        blackstar[to_1D_position(point)] = instruction.add;
+      });
+    }
+  }
+
+  [[nodiscard]] size_t to_1D_position(Point3D point) const {
+    return point.x
+      + (point.y) * universe.x.size()
+      + (point.z) * universe.x.size() * universe.y.size();
+  }
+
+  [[nodiscard]] long long int the_answer_is() const {
+    long long int c = 0;
+
+    universe.travel_the_universe([&](Point3D point) {
+      if (blackstar[to_1D_position(point)]) {
+        c += universe[point];
+      }
+    });
+
+    return c;
+  }
+};
 
 static constexpr size_t part_a_max_distance = 50 + 50 + 1;// from -50 to 50
 
-struct PartA {
+struct HelloDarkness {
+  [[nodiscard]] static long long int my_old_friend(
+    const std::vector<Instruction> & instructions
+  ) {
+    HelloDarkness instance;
+    for (const auto & instruction : instructions) {
+      instance.run(instruction);
+    }
+    return instance.how_many_lit();
+  }
+
   std::array<std::array<std::array<bool,
     part_a_max_distance>, part_a_max_distance>, part_a_max_distance
   > area;
 
-  PartA() {
+  HelloDarkness() {
     for (auto & xs : area) {
       for (auto & ys : xs) {
         ys.fill(false);
@@ -154,13 +264,15 @@ struct PartA {
   }
 
   void run(const Instruction & instruction) {
-    if (instruction.cube.is_init_protocole()) {
-      const auto & cube = instruction.cube;
-      for (int x = cube.x.min; x < cube.x.max_ex; ++x) {
-        for (int y = cube.y.min; y < cube.y.max_ex; ++y) {
-          for (int z = cube.z.min; z < cube.z.max_ex; ++z) {
-            area[x + 50][y + 50][z + 50] = instruction.add;
-          }
+    const auto & cube = instruction.cube;
+
+    if (cube.min.x < -50 || cube.min.y < -50 || cube.min.z < -50) return;
+    if (cube.min.x > 50 || cube.min.y > 50 || cube.min.z > 50) return;
+
+    for (int x = cube.min.x; x < cube.max.x; ++x) {
+      for (int y = cube.min.y; y < cube.max.y; ++y) {
+        for (int z = cube.min.z; z < cube.max.z; ++z) {
+          area[x + 50][y + 50][z + 50] = instruction.add;
         }
       }
     }
@@ -178,99 +290,10 @@ struct PartA {
   }
 };
 
-struct PartB {
-  std::map<Cube, std::vector<Cube>> managed_cubes = {};
-  std::set<Cube> state = {};
-
-  explicit PartB(std::vector<Cube> cubes) {
-    const auto original = cubes;
-    bool stable = false;
-    while (!stable) {
-      std::cout << cubes.size() << '\n';
-      stable = true;
-
-      for (size_t i_lhs = 0; i_lhs + 1 < cubes.size(); ++i_lhs) {
-        // std::cout << "lhs=" << i_lhs << '\n';
-
-        for (size_t i_rhs = i_lhs + 1; i_rhs < cubes.size(); ++i_rhs) {
-          // std::cout << "rhs=" << i_rhs << '\n';
-          const auto cut = Cube::ensure_distinct(cubes[i_lhs], cubes[i_rhs]);
-
-          if (cut.size() != 0) {
-            if (cut.size() < 2)  {
-              //std::cout << "bad fusion\n";
-              // std::cout << cubes[i_lhs] << " & " << cubes[i_rhs] << " made one \n";
-            } else if (cut.size() == 2) {
-              //std::cout << "bad cut\n";
-              //std::cout << cubes[i_lhs] << " & " << cubes[i_rhs] << " not equaldistinct but two blocks\n";
-              //std::cout << cut[0] << " & " << cut[1] << " not equaldistinct but two blocks\n";
-            } else {
-              //std::cout << cubes[i_lhs] << " & " << cubes[i_rhs] << " boom\n";
-              cubes[i_lhs] = cut[0];
-              cubes[i_rhs] = cut[1];
-              for (size_t i = 2; i != cut.size(); ++i) {
-                cubes.push_back(cut[i]);
-              }
-              //std::cout << "+" << cut.size() - 2 << '\n';
-
-              stable = false;
-            }
-          }
-        }
-      }
-    }
-
-    std::cout << "end\n";
-
-    //for (const auto cuber : cubes) {
-    //  std::cout << cuber << '\n';
-    //}
-
-    for (const auto cube : cubes) {
-      for (const auto ori : original) {
-        if (!ori.disjoint_to(cube)) {
-          managed_cubes[ori].push_back(cube);
-        }
-      }
-    }
-
-    std::cout << "true end\n";
-
-    std::cout << cubes.size() << '\n';
-  }
-
-  void run(const Instruction & instruction) {
-    const auto & cubes = managed_cubes.find(instruction.cube)->second;
-
-    for (const auto cube : cubes) {
-      if (instruction.add) {
-        state.emplace(cube);
-      } else {
-        const auto it = state.find(cube);
-        if (it != state.end()) {
-          //std::cout << "erased" << how_many_lit() << '\n';
-          state.erase(it);
-          //std::cout << "post erased" << how_many_lit() << '\n';
-        }
-      }
-    }
-  }
-
-
-  [[nodiscard]] long long int how_many_lit() const {
-    long long int total = 0;
-
-    for (const auto & cube : state) {
-      total += cube.volume();
-    }
-
-    return total;
-  }
-};
-
 
 
 Output day_2021_22(const std::vector<std::string> & lines, const DayExtraInfo &) {
+  // ==== Parsing
   const std::regex regex(R"((on|off) x=(-?[0-9]+)\.\.(-?[0-9]+),y=(-?[0-9]+)\.\.(-?[0-9]+),z=(-?[0-9]+)\.\.(-?[0-9]+))");
   std::smatch matches;
 
@@ -283,35 +306,26 @@ Output day_2021_22(const std::vector<std::string> & lines, const DayExtraInfo &)
       continue;
     }
 
-    Cube cube = Cube(
-      std::stoi(matches[2].str()),
-      std::stoi(matches[3].str()),
-      std::stoi(matches[4].str()),
-      std::stoi(matches[5].str()),
-      std::stoi(matches[6].str()),
-      std::stoi(matches[7].str())
-    );
+    Cuboid cube = Cuboid{
+      Point3D {
+        std::stoi(matches[2].str()),
+        std::stoi(matches[4].str()),
+        std::stoi(matches[6].str()),
+      },
+      
+      Point3D {
+        std::stoi(matches[3].str()) + 1,
+        std::stoi(matches[5].str()) + 1,
+        std::stoi(matches[7].str()) + 1,
+      }
+    };
 
     instructions.push_back(Instruction{ matches[1] == "on", { cube } });
   }
 
-  // Slow Part A
-  PartA slow_part_a;
-  for (const auto & instruction : instructions) {
-    slow_part_a.run(instruction);
-  }
+  // ==== Run
+  const long long int init = HelloDarkness::my_old_friend(instructions);
+  const long long int reload = HitTheLights::let_the_music_move_you(instructions);
 
-std::cout << "B\n";
-  // Part B
-  std::vector<Cube> cubes;
-  for (const auto & instruction : instructions) {
-    cubes.push_back(instruction.cube);
-  }
-std::cout << "ok\n";
-  PartB part_b(cubes);
-  for (const auto & instruction : instructions) {
-    part_b.run(instruction);
-  }
-
-  return Output(slow_part_a.how_many_lit(), part_b.how_many_lit());
+  return Output(init, reload);
 }
