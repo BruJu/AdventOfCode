281aba4496f5b2252e8991b03f6e66a699951808
Julian Bruyat <bruyat.ju@live.fr>
2022-12-01 06:14:48 +0100

    2022 01

--- src/2022/day01.cpp
+++ src/2022/day01.cpp
@@ -1,15 +1,41 @@
 #include "../advent_of_code.hpp"
+#include <algorithm>
 #include <numeric>
 
+// Given a list of list of numbers:
+// - (A) Find the max
+// - (B) Sum the 3 biggest values
+
+// Example of input:
+// 1
+// 2
+//
+// 5
+//
+// 6
+// 7
+// 2
+
 Output day_2022_01(const std::vector<std::string> & lines, const DayExtraInfo &) {
-  // std::vector<int> numbers = lines_transform::to_ints(lines);
+  // Process the input
+  std::vector<int> elves = lines_transform::group<int, int>(
+    /* input */           lines,
+    /* line convertion */ [](const std::string & s) { return std::stoi(s); },
+    /* reducer */         [](const int lhs, const int rhs) { return lhs + rhs; },
+    /* finalizer */       lines_transform::map_identity<int>
+  );
+
+  // Find the 3 maxes
+  const auto last_interesting_elve = elves.begin() + 3;
+  std::partial_sort(
+    elves.begin(), last_interesting_elve, elves.end(),
+    std::greater<int>{}
+  );
   
-  // Part A
-  size_t part_a = 0;
+  // Compute output
+  const size_t part_a = elves[0];
+  const size_t part_b = std::reduce(elves.begin(), last_interesting_elve);
 
-
-  // Part B
-  size_t part_b = 0;
-
+  // Done gg
   return Output(part_a, part_b);
 }
