c4c842b02ef4a03bdc7f0f26e6e075edb9c77e92
Julian Bruyat <bruyat.ju@live.fr>
2022-12-06 06:25:55 +0100

    2022 06

---
+++ src/2022/day06.cpp
@@ -0,0 +1,44 @@
+#include "../advent_of_code.hpp"
+#include <vector>
+#include <map>
+
+// https://adventofcode.com/2022/day/6
+// Find the first position where the n previous characters are distinct
+
+template<size_t PrefixSize>
+[[nodiscard]] size_t find_end_of_start_of_message(const std::string & line) {
+  if (line.size() < PrefixSize) return static_cast<size_t>(-1);
+  
+  std::map<char, int> occurrences;
+
+  for (size_t i = 0; i != PrefixSize - 1; ++i) {
+      ++occurrences[line[i]];
+  }
+
+  for (size_t i = PrefixSize - 1; i != line.size(); ++i) {
+    // Count new symbol
+    ++occurrences[line[i]];
+
+    if (occurrences.size() == PrefixSize) {
+      return i + 1;
+    }
+
+    // Remove symbol out of window
+    const char out_of_window_symbol = line[i - (PrefixSize - 1)];
+    const auto it_occurrences = occurrences.find(out_of_window_symbol);
+    --(it_occurrences->second);
+
+    if (it_occurrences->second == 0) {
+      occurrences.erase(it_occurrences);
+    }
+  }
+
+  return static_cast<size_t>(-1);
+}
+
+Output day_2022_06(const std::vector<std::string> & lines, const DayExtraInfo & dei) {
+  return Output(
+    dei.can_skip_part_A ? 0 : find_end_of_start_of_message< 4>(lines[0]),
+    dei.can_skip_part_B ? 0 : find_end_of_start_of_message<14>(lines[0])
+  );
+}
--- src/2022/days.hpp
+++ src/2022/days.hpp
@@ -5,8 +5,10 @@ Output day_2022_02(const std::vector<std::string> & lines, const DayExtraInfo &)
 Output day_2022_03(const std::vector<std::string> & lines, const DayExtraInfo &);
 Output day_2022_04(const std::vector<std::string> & lines, const DayExtraInfo &);
 Output day_2022_05(const std::vector<std::string> & lines, const DayExtraInfo &);
+Output day_2022_06(const std::vector<std::string> & lines, const DayExtraInfo &);
 
 static constexpr std::array<DayEntryPoint *, 25> days_2022 {
     &day_2022_01, &day_2022_02, &day_2022_03, &day_2022_04, &day_2022_05,
-    nullptr
+    &day_2022_06, nullptr
 };
+
