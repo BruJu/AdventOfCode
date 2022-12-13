9b80601c620fa9b3018c65d1412e4f96bbe117b1
Julian Bruyat <bruyat.ju@live.fr>
2022-12-13 07:25:46 +0100

    2022 13

--- src/2022/days.hpp
+++ src/2022/days.hpp
@@ -12,10 +12,11 @@ Output day_2022_09(const std::vector<std::string> & lines, const DayExtraInfo &)
 Output day_2022_10(const std::vector<std::string> & lines, const DayExtraInfo &);
 Output day_2022_11(const std::vector<std::string> & lines, const DayExtraInfo &);
 Output day_2022_12(const std::vector<std::string> & lines, const DayExtraInfo &);
+Output day_2022_13(const std::vector<std::string> & lines, const DayExtraInfo &);
 
 static constexpr std::array<DayEntryPoint *, 25> days_2022 {
     &day_2022_01, &day_2022_02, &day_2022_03, &day_2022_04, &day_2022_05,
     &day_2022_06, &day_2022_07, &day_2022_08, &day_2022_09, &day_2022_10,
-    &day_2022_11, &day_2022_12, nullptr
+    &day_2022_11, &day_2022_12, &day_2022_13, nullptr
 };
 
---
+++ src/util/nested_ints.hpp
@@ -0,0 +1,92 @@
+#include <vector>
+#include <string>
+#include <string_view>
+#include <stdexcept>
+#include <cstdint>
+#include <iostream>
+
+namespace bj {
+  namespace nested_ints {
+    struct NestedInts {
+      bool is_integer;
+      std::int64_t direct_int;
+      std::vector<NestedInts> nested;
+
+      explicit NestedInts(std::int64_t direct_int) : is_integer(true), direct_int(direct_int) {}
+      explicit NestedInts(std::vector<NestedInts> nested) : is_integer(false), direct_int(0), nested(std::move(nested)) {}
+
+      [[nodiscard]] bool operator==(const NestedInts & other) const {
+        if (is_integer != other.is_integer) return false;
+        if (is_integer) {
+          return direct_int == other.direct_int;
+        } else {
+          if (nested.size() != other.nested.size()) return false;
+          for (size_t i = 0; i != nested.size(); ++i) {
+            if (nested[i] != other.nested[i]) return false;
+          }
+          return true;
+        }
+      }
+
+      [[nodiscard]]bool operator!=(const NestedInts & other) const {
+        return !(operator==(other));
+      }
+
+      friend std::ostream & operator<<(std::ostream & stream, const NestedInts & self) {
+        if (self.is_integer) {
+          return stream << self.direct_int;
+        } else {
+          stream << '[';
+          for (size_t i = 0; i != self.nested.size(); ++i) {
+            if (i != 0) stream << ',';
+            stream << self.nested[i];
+          }
+          return stream << ']';
+        }
+      }
+    };
+
+    struct Reader {
+      std::string_view view;
+      size_t i = 0;
+
+      Reader(std::string_view view) : view(view) {}
+
+      [[nodiscard]] NestedInts next() {
+        if (view[i] >= '0' && view[i] <= '9') {
+          std::int64_t x = 0;
+
+          while (i <= view.size() && view[i] != ']' && view[i] != ',') {
+            x += x * 10 + (view[i] - '0');
+            ++i;
+          }
+
+          return NestedInts(x);
+        } else if (view[i] == '[') {
+          std::vector<NestedInts> values;
+
+          ++i; // consume [
+
+          while (view[i] != ']') {
+            values.emplace_back(next());
+
+            if (view[i] == ',') ++i;
+          }
+
+          ++i;  // consume ]
+          return NestedInts(values);
+        } else {
+          std::cerr << i << " " << view << "\n";
+          throw std::runtime_error("Bad string passed to nested_ints");
+        }
+      }
+    };
+
+    inline NestedInts read(const std::string & line) {
+      if (line.size() == 0) return NestedInts(0);
+      Reader reader(line);
+      return reader.next();
+    }
+  }
+
+}
