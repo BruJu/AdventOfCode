d0642dada05430e848063d2ef3b6682985112d41
Julian Bruyat <bruyat.ju@live.fr>
2022-12-18 13:37:38 +0100

    2022 18 Space exploration of the outside instead of cartography

--- src/2022/day18.cpp
+++ src/2022/day18.cpp
@@ -8,6 +8,8 @@
 
 // https://adventofcode.com/2022/day/18
 
+namespace {
+
 struct Cube {
   int x;
   int y;
@@ -15,39 +17,22 @@ struct Cube {
   
   friend auto operator<=>(const Cube &, const Cube &) = default;
 
-  std::array<Cube, 6> adj_cubes() const {
+  [[nodiscard]] std::array<Cube, 6> adj_cubes() const {
     return std::array<Cube, 6>{
       Cube{ x - 1, y, z },
       Cube{ x + 1, y, z },
-      Cube{ x, y - 1,z },
-      Cube{ x, y + 1,z },
+      Cube{ x, y - 1, z },
+      Cube{ x, y + 1, z },
       Cube{ x, y, z - 1 },
       Cube{ x, y, z + 1 }
     };
   }
 };
 
-class Cartography {
-public:
-  enum class Content { Cube, AirFree };
-
-private:
-
+std::pair<Cube, Cube> compute_bounds(const std::set<Cube> & cubes) {
   Cube min_bound {0,0,0};
   Cube max_bound {0,0,0};
 
-  std::map<Cube, Content> map_cubes;
-
-  void fill_the_map();
-
-public:
-  explicit Cartography(const std::set<Cube> & cubes);
-
-  [[nodiscard]] bool is_air_free(Cube cube) const;
-  [[nodiscard]] bool is_out_of_bound(Cube cube) const;
-};
-
-Cartography::Cartography(const std::set<Cube> & cubes) {
   for (const Cube & cube : cubes) {
     min_bound.x = std::min(min_bound.x, cube.x);
     min_bound.y = std::min(min_bound.y, cube.z);
@@ -56,8 +41,6 @@ Cartography::Cartography(const std::set<Cube> & cubes) {
     max_bound.x = std::max(max_bound.x, cube.x);
     max_bound.y = std::max(max_bound.y, cube.z);
     max_bound.z = std::max(max_bound.y, cube.z);
-
-    map_cubes.emplace(cube, Content::Cube);
   }
 
   --min_bound.x;
@@ -67,44 +50,10 @@ Cartography::Cartography(const std::set<Cube> & cubes) {
   ++max_bound.y;
   ++max_bound.z;
 
-  fill_the_map();
+  return { min_bound, max_bound };
 }
 
-void Cartography::fill_the_map() {
-  std::stack<Cube> to_visit;
-
-  const auto acc_cube = [&](const Cube cube) {
-    map_cubes.emplace(cube, Content::AirFree);
-    to_visit.emplace(cube);
-  };
-
-  for (const Cube adj : min_bound.adj_cubes()) {
-    if (!is_out_of_bound(adj) && !map_cubes.contains(adj)) {
-      acc_cube(adj);
-    }
-  }
-
-  while (!to_visit.empty()) {
-    const Cube next = to_visit.top();
-    to_visit.pop();
-
-    for (const Cube adj : next.adj_cubes()) {
-      if (!is_out_of_bound(adj) && !map_cubes.contains(adj)) {
-        acc_cube(adj);
-      }
-    }
-  }
-}
-
-bool Cartography::is_air_free(const Cube cube) const{
-  if (is_out_of_bound(cube)) return true;
-
-  const auto it = map_cubes.find(cube);
-  if (it == map_cubes.end()) return false;
-  return it->second == Content::AirFree;
-}
-
-bool Cartography::is_out_of_bound(const Cube cube) const {
+bool is_out_of_bound(const Cube & cube, const Cube & min_bound, const Cube & max_bound) {
   if (cube.x < min_bound.x) return true;
   if (cube.y < min_bound.y) return true;
   if (cube.z < min_bound.z) return true;
@@ -116,6 +65,27 @@ bool Cartography::is_out_of_bound(const Cube cube) const {
   return false;
 }
 
+struct CubeVisitor {
+  std::set<Cube> visited;
+  std::stack<Cube> to_visit;
+
+  void add_cube(const Cube & cube) {
+    if (visited.contains(cube)) return;
+    visited.emplace(cube);
+    to_visit.push(cube);
+  }
+
+  [[nodiscard]] std::optional<Cube> next() {
+    if (to_visit.empty()) return std::nullopt;
+    const Cube res = to_visit.top();
+    to_visit.pop();
+    return res;
+  }
+};
+
+}
+
+
 Output day_2022_18(const std::vector<std::string> & lines, const DayExtraInfo &) {
   std::set<Cube> cubes;
 
@@ -124,19 +94,31 @@ Output day_2022_18(const std::vector<std::string> & lines, const DayExtraInfo &)
     cubes.emplace(Cube { std::stoi(l[0]), std::stoi(l[1]), std::stoi(l[2]) });
   }
 
-  const Cartography carto(cubes);
-
   int surface_of_structure = 0;
-  int surface_to_air = 0;
-
   for (const Cube & cube : cubes) {
     for (const Cube & adj_cube : cube.adj_cubes()) {
-      if (cubes.contains(adj_cube)) {
+      if (!cubes.contains(adj_cube)) {
         ++surface_of_structure;
       }
+    }
+  }
 
-      if (carto.is_air_free(adj_cube)) {
+  const auto [min_bound, max_bound] = compute_bounds(cubes);
+
+  CubeVisitor visitor;
+  visitor.add_cube(min_bound);
+
+  int surface_to_air = 0;
+
+  while (const auto opt_cube = visitor.next()) {
+    for (const Cube & neighbour : opt_cube->adj_cubes()) {
+      if (is_out_of_bound(neighbour, min_bound, max_bound)) continue;
+      if (visitor.visited.contains(neighbour)) continue;
+
+      if (cubes.contains(neighbour)) {
         ++surface_to_air;
+      } else {
+        visitor.add_cube(neighbour);
       }
     }
   }
