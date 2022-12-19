af2d46fed1bbb8e1ce4a7c27abd02dbd1ab6c454
Julian Bruyat <bruyat.ju@live.fr>
2022-12-19 09:56:25 +0100

    2022 19 cleanup

--- src/2022/day19.cpp
+++ src/2022/day19.cpp
@@ -1,24 +1,19 @@
 #include "../advent_of_code.hpp"
-#include <array>
-#include <compare>
-#include <map>
-#include <ranges>
-#include <set>
-#include <stack>
 #include <regex>
+#include <stack>
 
 // https://adventofcode.com/2022/day/19
 
-struct EmptyStateClass {};
-EmptyStateClass EmptyState;
+namespace {
 
+// Resources
 struct Resource {
   int ore = 0;
   int clay = 0;
   int obsidian = 0;
   int geode = 0;
 
-  Resource & operator+=(const Resource & rhs) {
+  constexpr Resource & operator+=(const Resource & rhs) noexcept {
     ore += rhs.ore;
     clay += rhs.clay;
     obsidian += rhs.obsidian;
@@ -26,7 +21,13 @@ struct Resource {
     return *this;
   }
   
-  Resource & operator-=(const Resource & rhs) {
+  friend constexpr Resource operator+(const Resource & lhs, const Resource & rhs) noexcept {
+    Resource copy = lhs;
+    copy += rhs;
+    return copy;
+  }
+
+  constexpr Resource & operator-=(const Resource & rhs) noexcept {
     ore -= rhs.ore;
     clay -= rhs.clay;
     obsidian -= rhs.obsidian;
@@ -34,49 +35,47 @@ struct Resource {
     return *this;
   }
 
-  int how_much_can_fit(const Resource & rhs) {
-    int n = 999999;
-    if (rhs.ore      != 0) n = std::min(n, ore / rhs.ore);
-    if (rhs.clay     != 0) n = std::min(n, clay / rhs.clay);
-    if (rhs.obsidian != 0) n = std::min(n, obsidian / rhs.obsidian);
-    if (rhs.geode    != 0) n = std::min(n, geode / rhs.geode);
-    return n;
+  [[nodiscard]] constexpr bool are_all_higher_or_equal(const Resource & rhs) const noexcept {
+    return ore >= rhs.ore && clay >= rhs.clay && obsidian >= rhs.obsidian && geode >= rhs.geode;
   }
 
   friend std::ostream & operator<<(std::ostream & stream, const Resource & resource) {
     return stream
-      << "(o=" << resource.ore
-      << ", c=" << resource.clay
-      << ", b=" << resource.obsidian
-      << ", g=" << resource.geode
-      << ")";
+      << "[" << resource.ore << ", " << resource.clay
+      << ", " << resource.obsidian << ", " << resource.geode
+      << "]";
   }
 };
 
+namespace Robot {
+  constexpr Resource Ore      = Resource { 1, 0, 0, 0 };
+  constexpr Resource Clay     = Resource { 0, 1, 0, 0 };
+  constexpr Resource Obsidian = Resource { 0, 0, 1, 0 };
+  constexpr Resource Geode    = Resource { 0, 0, 0, 1 };
+}
+
+// A blueprint description
 struct Blueprint {
-  int ore_robot_cost_ore;
-  int clay_robot_cost_ore;
-  int obsidian_robot_cost_ore;
-  int obsidian_robot_cost_clay;
-  int geode_robot_cost_ore;
-  int geode_robot_cost_obsidian;
+  Resource for_ore;
+  Resource for_clay;
+  Resource for_obsidian;
+  Resource for_geode;
 };
 
+// Marker to stop default constructing something that should be copied
+struct EmptyStateClass {};
+EmptyStateClass EmptyState;
 
+// A state with the current robots, the resources and the time
 struct State {
   Resource current_robots;
   Resource resources;
   int time = 0;
 
-  explicit State([[maybe_unused]] EmptyStateClass marker) {
+  // Require to bring a marker to
+  // say "hey I really want to default construct this"
+  explicit State([[maybe_unused]] EmptyStateClass marker) noexcept {
     current_robots.ore = 1;
-  }
-
-  State do_nothing() const {
-    State copy = *this;
-    copy.resources += current_robots;
-    copy.time += 1;
-    return copy;
   }
 
   friend std::ostream & operator<<(std::ostream & stream, const State & state) {
@@ -91,95 +90,89 @@ struct State {
     if (auto next = buy_geode(blueprint)) output.emplace(next.value());
   }
 
-  [[nodiscard]] std::optional<State> try_buy(Resource what, const Resource & cost) const {
-    if (resources.ore      < cost.ore)      return std::nullopt;
-    if (resources.clay     < cost.clay)     return std::nullopt;
-    if (resources.obsidian < cost.obsidian) return std::nullopt;
-    if (resources.geode    < cost.geode)    return std::nullopt;
+private:
+  // Time brings all things to past
+  [[nodiscard]] State do_nothing() const noexcept {
+    State copy = *this;
+    copy.resources += current_robots;
+    copy.time += 1;
+    return copy;
+  }
+
+  // Returns a new state where we bought the wanted machine, or nullopt if could not
+  [[nodiscard]] std::optional<State> try_buy(const Resource & robot, const Resource & cost) const noexcept {
+    if (!resources.are_all_higher_or_equal(cost)) {
+      return std::nullopt;
+    }
 
     State copy = *this;
     copy.resources -= cost;
     copy.resources += copy.current_robots;
-    copy.current_robots += what;
+    copy.current_robots += robot;
     ++copy.time;
     return copy;
   }
 
-  [[nodiscard]] std::optional<State> buy_ore(const Blueprint & blueprint) const {
-    return try_buy(
-      Resource{ 1, 0, 0, 0 },
-      Resource{ blueprint.ore_robot_cost_ore }
-    );
+  [[nodiscard]] std::optional<State> buy_ore(const Blueprint & blueprint) const noexcept {
+    return try_buy(Robot::Ore, blueprint.for_ore);
   }
 
-  [[nodiscard]] std::optional<State> buy_clay(const Blueprint & blueprint) const {
-    return try_buy(
-      Resource{ 0, 1, 0, 0 },
-      Resource{ blueprint.clay_robot_cost_ore }
-    );
+  [[nodiscard]] std::optional<State> buy_clay(const Blueprint & blueprint) const noexcept {
+    return try_buy(Robot::Clay, blueprint.for_clay);
   }
   
-  [[nodiscard]] std::optional<State> buy_obsidian(const Blueprint & blueprint) const {
-    return try_buy(
-      Resource{ 0, 0, 1, 0 },
-      Resource{ blueprint.obsidian_robot_cost_ore, blueprint.obsidian_robot_cost_clay }
-    );
+  [[nodiscard]] std::optional<State> buy_obsidian(const Blueprint & blueprint) const noexcept {
+    return try_buy(Robot::Obsidian, blueprint.for_obsidian);
   }
   
-  [[nodiscard]] std::optional<State> buy_geode(const Blueprint & blueprint) const {
-    return try_buy(
-      Resource{ 0, 0, 0, 1 },
-      Resource{ blueprint.geode_robot_cost_ore, 0, blueprint.geode_robot_cost_obsidian }
-    );
+  [[nodiscard]] std::optional<State> buy_geode(const Blueprint & blueprint) const noexcept {
+    return try_buy(Robot::Geode, blueprint.for_geode);
   }
 
-  long long int get_possible_best(const Blueprint & blueprint, int max_minutes) const {
+public:
+  // Returns an evaluation of the possible best number of geodes. Fast and very approximate.
+  [[nodiscard]] long long int get_possible_best(const Blueprint & blueprint, int max_minutes) const noexcept {
     State cpy = *this;
 
     for (int i = time; i != max_minutes; ++i) {
       if (auto geode = cpy.buy_geode(blueprint)) {
+        // Can build a geode machine cool
         cpy = *geode;
       } else {
-        ++cpy.time;
+        // Exagerate an increase of capacity to be able to produce geode robots
         cpy.resources += cpy.current_robots;
-        ++cpy.current_robots.clay;
-        ++cpy.current_robots.ore;
-        ++cpy.current_robots.obsidian;
+        cpy.current_robots += Robot::Ore + Robot::Clay + Robot::Obsidian;
+        ++cpy.time;
       }
-      
     }
 
     return cpy.resources.geode;
   }
-
 };
 
-
-template<int max_minutes>
-int compute_blueprint_quality(const Blueprint & blueprint) {
+template<int MaxMinutes>
+[[nodiscard]] int find_max_possible_geodes(const Blueprint & blueprint) {
+  // DFS with state pruning
   std::stack<State> states;
   states.emplace(EmptyState);
 
-  int best_quality = 0;
+  int max_geodes = 0;
 
   while (!states.empty()) {
     const State state = states.top();
     states.pop();
 
-    if (best_quality >= state.get_possible_best(blueprint, max_minutes)) {
-      continue;
-    }
-
-    if (state.time == max_minutes) {
-      best_quality = std::max(best_quality, state.resources.geode);
-    } else  {
+    if (state.time == MaxMinutes) { // End of branch
+      max_geodes = std::max(max_geodes, state.resources.geode);
+    } else if (max_geodes < state.get_possible_best(blueprint, MaxMinutes)) { // State pruning
       state.add_next_states(states, blueprint);
     }
   }
 
-  return best_quality;
+  return max_geodes;
 }
 
+// Transform the input into blueprints
 std::vector<Blueprint> read_blueprints(const std::vector<std::string> & lines) {
   const char * regex_str = R"(^Blueprint (\d+): Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian.)";
   std::regex regex_ { regex_str };
@@ -194,12 +187,16 @@ std::vector<Blueprint> read_blueprints(const std::vector<std::string> & lines) {
 
     blueprint.emplace_back(
       Blueprint{
-        .ore_robot_cost_ore = std::stoi(smatch_[2].str()),
-        .clay_robot_cost_ore = std::stoi(smatch_[3].str()),
-        .obsidian_robot_cost_ore = std::stoi(smatch_[4].str()),
-        .obsidian_robot_cost_clay = std::stoi(smatch_[5].str()),
-        .geode_robot_cost_ore = std::stoi(smatch_[6].str()),
-        .geode_robot_cost_obsidian = std::stoi(smatch_[7].str())
+        .for_ore = Resource { .ore = std::stoi(smatch_[2].str()) },
+        .for_clay = Resource { .ore = std::stoi(smatch_[3].str()) },
+        .for_obsidian = Resource {
+          .ore = std::stoi(smatch_[4].str()),
+          .clay = std::stoi(smatch_[5].str())
+        },
+        .for_geode = Resource {
+          .ore = std::stoi(smatch_[6].str()),
+          .obsidian = std::stoi(smatch_[7].str())
+        }
       }
     );
   }
@@ -207,24 +204,27 @@ std::vector<Blueprint> read_blueprints(const std::vector<std::string> & lines) {
   return blueprint;
 }
 
+}
+
 Output day_2022_19(const std::vector<std::string> & lines, const DayExtraInfo &) {
+  // Read input
   std::vector<Blueprint> blueprints = read_blueprints(lines);
 
-  long long int quality_sum = 0;
-
+  // Part A, 24 minutes, all blueprints
+  long long int blueprint_quality_sum = 0;
   for (size_t i = 0; i != blueprints.size(); ++i) {
-    const long long int my_quality = compute_blueprint_quality<24>(blueprints[i]);
-    quality_sum += my_quality * (i + 1);
+    const long long int best_found_geodes = find_max_possible_geodes<24>(blueprints[i]);
+    blueprint_quality_sum += best_found_geodes * (i + 1);
   }
 
+  // Part B, 32 minutes, first 3 blueprints
+  if (blueprints.size() > 3) blueprints.erase(blueprints.begin() + 3, blueprints.end());
   long long int part_b = 1;
-  if (blueprints.size() > 3) {
-    blueprints.erase(blueprints.begin() + 3, blueprints.end());
-  }
   for (const Blueprint & blueprint : blueprints) {
-    const long long int my_quality = compute_blueprint_quality<32>(blueprint);
-    part_b *= my_quality;
+    const long long int best_found_geodes = find_max_possible_geodes<32>(blueprint);
+    part_b *= best_found_geodes;
   }
 
-  return Output(quality_sum, part_b);
+  // kthxbye
+  return Output(blueprint_quality_sum, part_b);
 }
