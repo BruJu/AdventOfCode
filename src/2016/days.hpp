3b403f44bb0c7eb438292d823652a61ff2ee9100
Julian Bruyat <bruyat.ju@live.fr>
2022-12-24 21:39:49 +0100

    2016 25

--- src/2016/days.hpp
+++ src/2016/days.hpp
@@ -24,12 +24,12 @@ Output day_2016_21(const std::vector<std::string> & lines, const DayExtraInfo &)
 Output day_2016_22(const std::vector<std::string> & lines, const DayExtraInfo &);
 Output day_2016_23(const std::vector<std::string> & lines, const DayExtraInfo &);
 Output day_2016_24(const std::vector<std::string> & lines, const DayExtraInfo &);
+Output day_2016_25(const std::vector<std::string> & lines, const DayExtraInfo &);
 
 static constexpr std::array<DayEntryPoint *, 25> days_2016 {
     &day_2016_01, &day_2016_02, &day_2016_03, &day_2016_04, &day_2016_05,
     &day_2016_06, &day_2016_07, &day_2016_08, &day_2016_09, &day_2016_10,
     &day_2016_11, &day_2016_12, &day_2016_13, &day_2016_14, &day_2016_15,
     &day_2016_16, &day_2016_17, &day_2016_18, &day_2016_19, &day_2016_20,
-    &day_2016_21, &day_2016_22, &day_2016_23, &day_2016_24,
-    nullptr
+    &day_2016_21, &day_2016_22, &day_2016_23, &day_2016_24, &day_2016_25
 };
