f88a9fbdb05eec5e8a6baa8b91ae5774f4fbe514
Julian Bruyat <julian@bruy.at>
2023-12-15 06:49:08 +0100

    2021 03 easier to read code

--- 2021/src/day03.cpp
+++ 2021/src/day03.cpp
@@ -2,60 +2,45 @@
 #include <algorithm>
 #include <optional>
 
-template<typename Callable>
-static inline void for_each_bit(size_t nb_of_bits, Callable callable) {
-  for (size_t i = 0; i != nb_of_bits; ++i) {
-    const auto as_int = static_cast<int>(nb_of_bits - i) - 1;
-    const auto bit = 1 << as_int;
-    callable(i, bit);
+static int to_int(const std::string & str) {
+  int val = 0;
+  for (const char c : str) {
+    val = val * 2 + (c == '1' ? 1 : 0);
   }
+  return val;
 }
 
-static std::optional<int> search_rating(const std::vector<int> & values, int higher_power_of_2, bool search_most_common) {
-  std::vector<int> remaing_values = values;
-  int current_bit = higher_power_of_2;
-
-  while (remaing_values.size() > 1 && current_bit != 0) {
-    const size_t nb_of_values_with_current_bit = std::count_if(
-      remaing_values.begin(),
-      remaing_values.end(),
-      [current_bit](int value) { return value & current_bit; }
-    );
-    
-    const size_t nb_of_values_without_current_bit = remaing_values.size() - nb_of_values_with_current_bit;
-    const bool most_common_is_1 = nb_of_values_with_current_bit >= nb_of_values_without_current_bit;
-
-    remaing_values.erase(
-      std::remove_if(
-        remaing_values.begin(),
-        remaing_values.end(),
-          [&](int value) {
-              const bool has_bit_on_one = value & current_bit;
-              const bool is_most_common = has_bit_on_one == most_common_is_1;
-              const bool is_what_we_want = is_most_common == search_most_common;
-              return is_what_we_want;
-          }
-      ),
-      remaing_values.end()
-    );
-
-    current_bit /= 2;
+static std::optional<int> search_rating(const std::vector<std::string> & values, size_t pos, bool search_most_common) {
+  if (values.empty())          return std::nullopt;
+  if (values.size() == 1)      return to_int(values[0]);
+  if (pos == values[0].size()) return std::nullopt;
+  
+  size_t number_of_1 = 0;
+  for (const std::string & value : values) {
+    if (value[pos] == '1') {
+      ++number_of_1;
+    }
   }
 
-  if (remaing_values.size() != 1) return std::nullopt;
-  return remaing_values[0];
+  const size_t number_of_0 = values.size() - number_of_1;
+
+  const bool keep_1 = 
+    (search_most_common && number_of_1 >= number_of_0)
+    || (!search_most_common && number_of_1 < number_of_0)
+  ;
+
+  std::vector<std::string> filtered;
+  for (const std::string & value : values) {
+    if ((value[pos] == '1') == (keep_1)) {
+      filtered.emplace_back(value);
+    }
+  }
+
+  return search_rating(filtered, pos + 1, search_most_common);
 }
 
 Output day_2021_03(const std::vector<std::string> & lines, const DayExtraInfo &) {
   const int nb_of_bits = static_cast<int>(lines[0].size());
-
-  const auto input = lines_transform::map<int>(lines, [](const std::string & as_string) {
-    int as_int = 0;
-    for (const auto c : as_string) {
-      as_int = as_int * 2 + (c == '1' ? 1 : 0);
-    }
-    return as_int;
-  });
 
   // Part A
   auto values_per_bit = std::vector<size_t>(nb_of_bits);
@@ -72,19 +57,20 @@ Output day_2021_03(const std::vector<std::string> & lines, const DayExtraInfo &)
   int epsilon = 0;
   const size_t half = lines.size() / 2;
 
-  for_each_bit(nb_of_bits,
-    [&](size_t i, int bit) {
-      if (values_per_bit[i] > half) {
-        gamma = gamma | bit;
-      } else {
-        epsilon = epsilon | bit;
-      }
+  for (int i = 0; i != nb_of_bits; ++i) {
+    gamma *= 2;
+    epsilon *= 2;
+
+    if (values_per_bit[i] > half) {
+      gamma += 1;
+    } else {
+      epsilon += 1;
     }
-  );
+  }
 
   // Part B
-  const auto oxygen = search_rating(input, 1 << (nb_of_bits - 1), true);
-  const auto co2 = search_rating(input, 1 << (nb_of_bits - 1), false);
+  const auto oxygen = search_rating(lines, 0, true );
+  const auto co2    = search_rating(lines, 0, false);
 
   return Output(gamma * epsilon, *oxygen**co2);
 }
