1ad51987e112c33d0b32967ff8d07685b2c79496
Julian Bruyat <julian@bruy.at>
2025-08-15 02:01:30 +0200

    2024 23 Part B

--- 2024/src/day23.cpp
+++ 2024/src/day23.cpp
@@ -9,6 +9,7 @@
 
 #include <ctime>
 #include <random>
+#include "../../util/libs_ensemblist.hpp"
 
 // https://adventofcode.com/2024/day/23
 
@@ -31,9 +32,9 @@ int do_part_a(std::map<std::string, std::set<std::string>> & links) {
         if (!bs.contains(c)) continue;
 
         if (a[0] == 't' || b[0] == 't' || c[0] == 't') {
-          if (links.size() < 50) {
-            printf("%s %s %s\n", a.c_str(), b.c_str(), c.c_str());
-          }
+          // if (links.size() < 50) {
+          //   printf("%s %s %s\n", a.c_str(), b.c_str(), c.c_str());
+          // }
           ++part_a;
         }
       }
@@ -44,99 +45,98 @@ int do_part_a(std::map<std::string, std::set<std::string>> & links) {
   return part_a;
 }
 
-std::vector<std::set<std::string>> grow(
-  const std::vector<std::set<std::string>> & previous_cliques,
+// https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm
+void
+bronKerbosch1(
+  std::vector<std::set<std::string>> & maximal_cliques,
+  std::set<std::string> R,
+  std::set<std::string> P,
+  std::set<std::string> X,
   std::map<std::string, std::set<std::string>> & links
-  ) {
-  std::vector<std::set<std::string>> new_cliques;
-
-  for (const std::set<std::string> & clique : previous_cliques) {
-    const std::string elem = *clique.begin();
-
-    for (const std::string & other : links[elem]) {
-      if (clique.contains(other)) continue;
-
-      if (std::any_of(clique.begin(), clique.end(),
-        [&](const std::string & e) {
-          return !links[e].contains(other);
-        }
-      )) continue;
-
-      auto copy = clique;
-      copy.emplace(other);
-      new_cliques.emplace_back(copy);
-    }
+) {
+  if (P.empty() && X.empty()) {
+    maximal_cliques.emplace_back(R);
+    return;
   }
-  
-  printf("%zu\n", new_cliques.size());
 
-  return new_cliques;
+  for (auto it_P = P.begin(); it_P != P.end(); ++it_P) {
+    const std::string & vertex = *it_P;
+
+    // 1- Prepare recursive call
+
+    std::set<std::string> next_R = R; next_R.emplace(vertex);
+    
+    std::set<std::string> next_P(it_P, P.end());
+    std::set<std::string> next_X = X;
+    
+    auto it_links = links.find(vertex);
+    if (it_links != links.end()) {
+      next_P &= it_links->second;
+      next_X &= it_links->second;
+    } else {
+      next_P.clear();
+      next_X.clear();
+    }
+
+    // 2- Recursive call
+    bronKerbosch1(
+      maximal_cliques,
+      next_R, next_P, next_X, links
+    );
+
+    // 3- Move on
+
+    // P.erase(vertex); -> this is performed automtically because we only consider [it_P, P.end() [
+    X.emplace(vertex);
+  }
 }
-
 
 std::string do_part_b(std::map<std::string, std::set<std::string>> & links) {
+  // 1- Prepare parameters for Bron Kerbosch  
+  std::vector<std::set<std::string>> maximal_cliques;
+  std::set<std::string> R;
+  std::set<std::string> P;
+  std::set<std::string> X;
 
-  std::vector<std::set<std::string>> cliques;
-
-  for (const auto & [a, _] : links) {
-    std::set<std::string> singleton;
-    singleton.emplace(a);
-    cliques.emplace_back(singleton);
+  for (const auto & link : links) {
+    P.emplace(link.first);
   }
 
-  if (cliques.empty()) return "";
+  // 2- Bron Kerbosch call
+  bronKerbosch1(maximal_cliques, R, P, X, links);
 
-  while (true) {
-    if (cliques.size() > 1000) {
-      return "let's move on to the next puzzle";
+  // 3- Compute output
+  std::set<std::string> biggest_clique;
+  for (const auto & clique : maximal_cliques) {
+    if (clique.size() > biggest_clique.size()) {
+      biggest_clique = clique;
     }
-
-    std::vector<std::set<std::string>> new_cliques = grow(cliques, links);
-
-    if (new_cliques.size() == 0) {
-      std::string names;
-      for (const std::string & s : *cliques.begin()) {
-        names += ",";
-        names += s;
-      }
-      return names.substr(1);
-    }
-
-    cliques = std::move(new_cliques);
   }
+
+  std::string str;
+  for (const std::string & node : biggest_clique) {
+    if (!str.empty()) str += ',';
+    str += node;
+  }
+
+  return str;
 }
-
-
-
-
-
-
-
-
-
 
 }
 
-Output day_2024_23(const std::vector<std::string> & lines, const DayExtraInfo & dei) {
+Output day_2024_23(const std::vector<std::string> & lines, const DayExtraInfo &) {
   std::map<std::string, std::set<std::string>> links;
 
   for (const std::string & line : lines) {
     std::string a = line.substr(0, 2);
     std::string b = line.substr(3);
 
-    
     links[a].emplace(b);
     links[b].emplace(a);
   }
 
-  int part_a = do_part_a(links);
+  const int part_a = do_part_a(links);
+  const std::string part_b = do_part_b(links);
 
-  std::string part_b = do_part_b(links);
-
-
-
-
-  return Output(
-    part_a, part_b
-    );
+  return Output(part_a, part_b);
 }
